be994951098aca2d35d4c40f62c919883f823570
Tim Wood <washwithcare@gmail.com>
2011-03-07 18:36:24 -0800

    Switched to yyyy-MM-DD format for original proprietary format

--- test/date.js
+++ test/date.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
   test("dateFormat", function() {
 	var dateTest = new Date(2010, 1, 14, 15, 25, 50, 125);
     expect(13);
-	equal(dateTest.humanize("w, l D1 Y, h:m2:s2 a"), "Sunday, February 14th 2010, 3:25:50 pm");
-	equal(dateTest.humanize("w1, hA"), "Sun, 3PM");
-	equal(dateTest.humanize("L L1 L2 L3 l l1"), "2 2nd 02 02nd February Feb");
-	equal(dateTest.humanize("Y Y1"), "2010 10");
-	equal(dateTest.humanize("D D1 D2 D3"), "14 14th 14 14th");
-	equal(dateTest.humanize("W W1 W2 W3 w w1"), "0 0th 00 00th Sunday Sun");
-	equal(dateTest.humanize("d d1 d2 d3"), "45 45th 045 045th");
-	equal(dateTest.humanize("K K1 K2 K3"), "8 8th 08 08th");
-	equal(dateTest.humanize("h h1 h2 h3"), "3 3rd 03 03rd");
-	equal(dateTest.humanize("H H1 H2 H3"), "15 15th 15 15th");
-	equal(dateTest.humanize("m m1 m2 m3"), "25 25th 25 25th");
-	equal(dateTest.humanize("s s1 s2 s3"), "50 50th 50 50th");
+	equal(dateTest.humanize("dddd, MMMM Do YYYY, h:mm:ss a"), "Sunday, February 14th 2010, 3:25:50 pm");
+	equal(dateTest.humanize("ddd, hA"), "Sun, 3PM");
+	equal(dateTest.humanize("M Mo MM MMMM MMM"), "2 2nd 02 February Feb");
+	equal(dateTest.humanize("YYYY YY"), "2010 10");
+	equal(dateTest.humanize("D Do DD"), "14 14th 14");
+	equal(dateTest.humanize("d do dd dddd ddd"), "0 0th 00 Sunday Sun");
+	equal(dateTest.humanize("DDD DDDo DDDD"), "45 45th 045");
+	equal(dateTest.humanize("w wo ww"), "8 8th 08");
+	equal(dateTest.humanize("h hh"), "3 03");
+	equal(dateTest.humanize("H HH"), "15 15");
+	equal(dateTest.humanize("m mm"), "25 25");
+	equal(dateTest.humanize("s ss"), "50 50");
 	equal(dateTest.humanize("a A"), "pm PM");
   });
 
