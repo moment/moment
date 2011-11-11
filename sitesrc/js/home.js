df97d17316442ea73979a0706cf285121f984889
Tim Wood <washwithcare@gmail.com>
2011-11-11 08:08:40 -0800

    Fixing example format for French language #48

--- sitesrc/js/home.js
+++ sitesrc/js/home.js
@@ -4,5 +4,5 @@ $('#js-format-now').html('"' + moment().format('dddd, MMMM Do YYYY, h:mm:ss a') 
 $('#js-from-now').html('"' + moment([2011, 10, 30]).fromNow() + '"');
 $('#js-add').html('"' + moment().add('days', 9).format('dddd, MMMM Do YYYY') + '"');
 moment.lang('fr');
-$('#js-lang').html('"' + moment().format('dddd, MMMM Do YYYY, h:mm:ss a') + '"');
+$('#js-lang').html('"' + moment().format('LLLL') + '"');
 moment.lang('en');
