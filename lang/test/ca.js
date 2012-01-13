fea3266a1675f514bfcfc30b8f9a8415302f69cd
Tim Wood <washwithcare@gmail.com>
2012-01-13 08:54:26 -0800

    Adding Catalan (ca) locale
    #129

---
+++ lang/test/ca.js
@@ -0,0 +1,198 @@
+
+/**************************************************
+  Català
+ *************************************************/
+
+module("lang:ca");
+
+test("parse", 96, function() {
+    moment.lang('ca');
+
+    var tests = "Gener Gen._Febrer Febr._Març Mar._Abril Abr._Maig Mai._Juny Jun._Juliol Jul._Agost Ag._Setembre Set._Octubre Oct._Novembre Nov._Desembre Des.".split("_");
+
+    var i;
+    function equalTest(input, mmm, i) {
+        equal(moment(input, mmm).month(), i, input + ' should be month ' + (i + 1));
+    }
+    for (i = 0; i < 12; i++) {
+        tests[i] = tests[i].split(' ');
+        equalTest(tests[i][0], 'MMM', i);
+        equalTest(tests[i][1], 'MMM', i);
+        equalTest(tests[i][0], 'MMMM', i);
+        equalTest(tests[i][1], 'MMMM', i);
+        equalTest(tests[i][0].toLocaleLowerCase(), 'MMMM', i);
+        equalTest(tests[i][1].toLocaleLowerCase(), 'MMMM', i);
+        equalTest(tests[i][0].toLocaleUpperCase(), 'MMMM', i);
+        equalTest(tests[i][1].toLocaleUpperCase(), 'MMMM', i);
+    }
+});
+
+test("format ordinal", 31, function() {
+    moment.lang('ca');
+    equal(moment([2011, 0, 1]).format('DDDo'), '1º', '1º');
+    equal(moment([2011, 0, 2]).format('DDDo'), '2º', '2º');
+    equal(moment([2011, 0, 3]).format('DDDo'), '3º', '3º');
+    equal(moment([2011, 0, 4]).format('DDDo'), '4º', '4º');
+    equal(moment([2011, 0, 5]).format('DDDo'), '5º', '5º');
+    equal(moment([2011, 0, 6]).format('DDDo'), '6º', '6º');
+    equal(moment([2011, 0, 7]).format('DDDo'), '7º', '7º');
+    equal(moment([2011, 0, 8]).format('DDDo'), '8º', '8º');
+    equal(moment([2011, 0, 9]).format('DDDo'), '9º', '9º');
+    equal(moment([2011, 0, 10]).format('DDDo'), '10º', '10º');
+
+    equal(moment([2011, 0, 11]).format('DDDo'), '11º', '11º');
+    equal(moment([2011, 0, 12]).format('DDDo'), '12º', '12º');
+    equal(moment([2011, 0, 13]).format('DDDo'), '13º', '13º');
+    equal(moment([2011, 0, 14]).format('DDDo'), '14º', '14º');
+    equal(moment([2011, 0, 15]).format('DDDo'), '15º', '15º');
+    equal(moment([2011, 0, 16]).format('DDDo'), '16º', '16º');
+    equal(moment([2011, 0, 17]).format('DDDo'), '17º', '17º');
+    equal(moment([2011, 0, 18]).format('DDDo'), '18º', '18º');
+    equal(moment([2011, 0, 19]).format('DDDo'), '19º', '19º');
+    equal(moment([2011, 0, 20]).format('DDDo'), '20º', '20º');
+
+    equal(moment([2011, 0, 21]).format('DDDo'), '21º', '21º');
+    equal(moment([2011, 0, 22]).format('DDDo'), '22º', '22º');
+    equal(moment([2011, 0, 23]).format('DDDo'), '23º', '23º');
+    equal(moment([2011, 0, 24]).format('DDDo'), '24º', '24º');
+    equal(moment([2011, 0, 25]).format('DDDo'), '25º', '25º');
+    equal(moment([2011, 0, 26]).format('DDDo'), '26º', '26º');
+    equal(moment([2011, 0, 27]).format('DDDo'), '27º', '27º');
+    equal(moment([2011, 0, 28]).format('DDDo'), '28º', '28º');
+    equal(moment([2011, 0, 29]).format('DDDo'), '29º', '29º');
+    equal(moment([2011, 0, 30]).format('DDDo'), '30º', '30º');
+
+    equal(moment([2011, 0, 31]).format('DDDo'), '31º', '31º');
+});
+
+test("format month", 12, function() {
+    moment.lang('ca');
+    var expected = "Gener Gen._Febrer Febr._Març Mar._Abril Abr._Maig Mai._Juny Jun._Juliol Jul._Agost Ag._Setembre Set._Octubre Oct._Novembre Nov._Desembre Des.".split("_");
+    var i;
+    for (i = 0; i < expected.length; i++) {
+        equal(moment([2011, i, 0]).format('MMMM MMM'), expected[i], expected[i]);
+    }
+});
+
+test("format week", 7, function() {
+    moment.lang('ca');
+    var expected = "Diumenge Dg._Dilluns Dl._Dimarts Dt._Dimecres Dc._Dijous Dj._Divendres Dv._Dissabte Ds.".split("_");
+
+    var i;
+    for (i = 0; i < expected.length; i++) {
+        equal(moment([2011, 0, 2 + i]).format('dddd ddd'), expected[i], expected[i]);
+    }
+});
+
+test("from", 30, function() {
+    moment.lang('ca');
+    var start = moment([2007, 1, 28]);
+    equal(start.from(moment([2007, 1, 28]).add({s:44}), true),  "uns segons", "44 seconds = a few seconds");
+    equal(start.from(moment([2007, 1, 28]).add({s:45}), true),  "un minut",      "45 seconds = a minute");
+    equal(start.from(moment([2007, 1, 28]).add({s:89}), true),  "un minut",      "89 seconds = a minute");
+    equal(start.from(moment([2007, 1, 28]).add({s:90}), true),  "2 minuts",     "90 seconds = 2 minutes");
+    equal(start.from(moment([2007, 1, 28]).add({m:44}), true),  "44 minuts",    "44 minutes = 44 minutes");
+    equal(start.from(moment([2007, 1, 28]).add({m:45}), true),  "una hora",       "45 minutes = an hour");
+    equal(start.from(moment([2007, 1, 28]).add({m:89}), true),  "una hora",       "89 minutes = an hour");
+    equal(start.from(moment([2007, 1, 28]).add({m:90}), true),  "2 hores",       "90 minutes = 2 hours");
+    equal(start.from(moment([2007, 1, 28]).add({h:5}), true),   "5 hores",       "5 hours = 5 hours");
+    equal(start.from(moment([2007, 1, 28]).add({h:21}), true),  "21 hores",      "21 hours = 21 hours");
+    equal(start.from(moment([2007, 1, 28]).add({h:22}), true),  "un dia",         "22 hours = a day");
+    equal(start.from(moment([2007, 1, 28]).add({h:35}), true),  "un dia",         "35 hours = a day");
+    equal(start.from(moment([2007, 1, 28]).add({h:36}), true),  "2 dies",        "36 hours = 2 days");
+    equal(start.from(moment([2007, 1, 28]).add({d:1}), true),   "un dia",         "1 day = a day");
+    equal(start.from(moment([2007, 1, 28]).add({d:5}), true),   "5 dies",        "5 days = 5 days");
+    equal(start.from(moment([2007, 1, 28]).add({d:25}), true),  "25 dies",       "25 days = 25 days");
+    equal(start.from(moment([2007, 1, 28]).add({d:26}), true),  "un mes",       "26 days = a month");
+    equal(start.from(moment([2007, 1, 28]).add({d:30}), true),  "un mes",       "30 days = a month");
+    equal(start.from(moment([2007, 1, 28]).add({d:45}), true),  "un mes",       "45 days = a month");
+    equal(start.from(moment([2007, 1, 28]).add({d:46}), true),  "2 mesos",      "46 days = 2 months");
+    equal(start.from(moment([2007, 1, 28]).add({d:74}), true),  "2 mesos",      "75 days = 2 months");
+    equal(start.from(moment([2007, 1, 28]).add({d:76}), true),  "3 mesos",      "76 days = 3 months");
+    equal(start.from(moment([2007, 1, 28]).add({M:1}), true),   "un mes",       "1 month = a month");
+    equal(start.from(moment([2007, 1, 28]).add({M:5}), true),   "5 mesos",      "5 months = 5 months");
+    equal(start.from(moment([2007, 1, 28]).add({d:344}), true), "11 mesos",     "344 days = 11 months");
+    equal(start.from(moment([2007, 1, 28]).add({d:345}), true), "un any",        "345 days = a year");
+    equal(start.from(moment([2007, 1, 28]).add({d:547}), true), "un any",        "547 days = a year");
+    equal(start.from(moment([2007, 1, 28]).add({d:548}), true), "2 anys",       "548 days = 2 years");
+    equal(start.from(moment([2007, 1, 28]).add({y:1}), true),   "un any",        "1 year = a year");
+    equal(start.from(moment([2007, 1, 28]).add({y:5}), true),   "5 anys",       "5 years = 5 years");
+});
+
+test("suffix", 2, function() {
+    moment.lang('ca');
+    equal(moment(30000).from(0), "en uns segons",  "prefix");
+    equal(moment(0).from(30000), "fa uns segons", "suffix");
+});
+
+
+test("now from now", 1, function() {
+    moment.lang('ca');
+    equal(moment().fromNow(), "fa uns segons",  "now from now should display as in the past");
+});
+
+
+test("fromNow", 2, function() {
+    moment.lang('ca');
+    equal(moment().add({s:30}).fromNow(), "en uns segons", "en uns segons");
+    equal(moment().add({d:5}).fromNow(), "en 5 dies", "en 5 dies");
+});
+
+
+test("calendar day", 7, function() {
+    moment.lang('ca');
+
+    var a = moment().hours(2).minutes(0).seconds(0);
+
+    equal(moment(a).calendar(),                         "avui a les 2:00",     "today at the same time");
+    equal(moment(a).add({ m: 25 }).calendar(),          "avui a les 2:25",     "Now plus 25 min");
+    equal(moment(a).add({ h: 1 }).calendar(),           "avui a les 3:00",     "Now plus 1 hour");
+    equal(moment(a).add({ d: 1 }).calendar(),           "demá a les 2:00",  "tomorrow at the same time");
+    equal(moment(a).add({ d: 1, h : -1 }).calendar(),   "demá a la 1:00",   "tomorrow minus 1 hour");
+    equal(moment(a).subtract({ h: 1 }).calendar(),      "avui a la 1:00",      "Now minus 1 hour");
+    equal(moment(a).subtract({ d: 1 }).calendar(),      "ahir a les 2:00",    "yesterday at the same time");
+});
+
+test("calendar next week", 15, function() {
+    moment.lang('ca');
+
+    var i;
+    var m;
+
+    for (i = 2; i < 7; i++) {
+        m = moment().add({ d: i });
+        equal(m.calendar(),       m.format('dddd [a ' + ((m.hours() !== 1) ? 'les' : 'la') + '] LT'),  "Today + " + i + " days current time");
+        m.hours(0).minutes(0).seconds(0).milliseconds(0);
+        equal(m.calendar(),       m.format('dddd [a ' + ((m.hours() !== 1) ? 'les' : 'la') + '] LT'),  "Today + " + i + " days beginning of day");
+        m.hours(23).minutes(59).seconds(59).milliseconds(999);
+        equal(m.calendar(),       m.format('dddd [a ' + ((m.hours() !== 1) ? 'les' : 'la') + '] LT'),  "Today + " + i + " days end of day");
+    }
+});
+
+test("calendar last week", 15, function() {
+    moment.lang('ca');
+
+    for (i = 2; i < 7; i++) {
+        m = moment().subtract({ d: i });
+        equal(m.calendar(),       m.format('[el] dddd [passat a ' + ((m.hours() !== 1) ? 'les' : 'la') + '] LT'),  "Today - " + i + " days current time");
+        m.hours(0).minutes(0).seconds(0).milliseconds(0);
+        equal(m.calendar(),       m.format('[el] dddd [passat a ' + ((m.hours() !== 1) ? 'les' : 'la') + '] LT'),  "Today - " + i + " days beginning of day");
+        m.hours(23).minutes(59).seconds(59).milliseconds(999);
+        equal(m.calendar(),       m.format('[el] dddd [passat a ' + ((m.hours() !== 1) ? 'les' : 'la') + '] LT'),  "Today - " + i + " days end of day");
+    }
+});
+
+test("calendar all else", 4, function() {
+    moment.lang('ca');
+    var weeksAgo = moment().subtract({ w: 1 });
+    var weeksFromNow = moment().add({ w: 1 });
+
+    equal(weeksAgo.calendar(),       weeksAgo.format('L'),  "1 week ago");
+    equal(weeksFromNow.calendar(),   weeksFromNow.format('L'),  "in 1 week");
+
+    weeksAgo = moment().subtract({ w: 2 });
+    weeksFromNow = moment().add({ w: 2 });
+
+    equal(weeksAgo.calendar(),       weeksAgo.format('L'),  "2 weeks ago");
+    equal(weeksFromNow.calendar(),   weeksFromNow.format('L'),  "in 2 weeks");
+});
