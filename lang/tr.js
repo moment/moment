f1acd0da2f23809418f4f2993c0a8befbfa3b904
Erhan Gundogan <erhan@trposta.net>
2012-01-14 00:39:57 +0200

    Turkish language files addes.

---
+++ lang/tr.js
@@ -0,0 +1,60 @@
+(function () {
+    var lang = {
+            months : "Ocak_Şubat_Mart_Nisan_Mayıs_Haziran_Temmuz_Ağustos_Eylül_Ekim_Kasım_Aralık".split("_"),
+            monthsShort : "Oca_Şub_Mar_Nis_May_Haz_Tem_Ağu_Eyl_Eki_Kas_Ara".split("_"),
+            weekdays : "Pazar_Pazartesi_Salı_Çarşamba_Perşembe_Cuma_Cumartesi".split("_"),
+            weekdaysShort : "Paz_Pts_Sal_Çar_Per_Cum_Cts".split("_"),
+            longDateFormat : {
+                LT : "HH:mm",
+                L : "DD.MM.YYYY",
+                LL : "D MMMM YYYY",
+                LLL : "D MMMM YYYY LT",
+                LLLL : "dddd, D MMMM YYYY LT"
+            },
+            meridiem : {
+                AM : 'AM',
+                am : 'am',
+                PM : 'PM',
+                pm : 'pm'
+            },
+            calendar : {
+                sameDay : '[bugün saat] LT',
+                nextDay : '[yarın saat] LT',
+                nextWeek : '[haftaya] dddd [saat] LT', 
+                lastDay : '[dün] LT',
+                lastWeek : '[geçen hafta] dddd [saat] LT', 
+                sameElse : 'L'
+            },
+            relativeTime : {
+                future : "%s sonra",
+                past : "%s önce",
+                s : "birkaç saniye",
+                m : "bir dakika",
+                mm : "%d dakika",
+                h : "bir saat",
+                hh : "%d saat",
+                d : "bir gün",
+                dd : "%d gün",
+                M : "bir ay",
+                MM : "%d ay",
+                y : "bir yıl",
+                yy : "%d yıl"
+            },
+            ordinal : function (number) {
+                var b = number % 10;
+                return (~~ (number % 100 / 10) === 1) ? 'th' :
+                    (b === 1) ? 'st' :
+                    (b === 2) ? 'nd' :
+                    (b === 3) ? 'rd' : 'th';
+            }
+        };
+
+    // Node
+    if (typeof module !== 'undefined') {
+        module.exports = lang;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.moment && this.moment.lang) {
+        this.moment.lang('tr', lang);
+    }
+}());
