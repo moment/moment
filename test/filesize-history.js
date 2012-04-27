d0bcc9aa70a1f0871d0b106eba1e66bf510d2d54
Tim Wood <washwithcare@gmail.com>
2012-04-26 17:32:52 -0700

    Adding 1.6.0 and 1.6.1 to the file size history script

--- test/filesize-history.js
+++ test/filesize-history.js
@@ -80,7 +80,7 @@ function getSizeAtVersion(version, path) {
 
 (function(){
 	var old_versions = '1.0.1 1.1.0 1.1.1 1.1.2 1.2.0 1.3.0 1.4.0'.split(' ');
-	var new_versions = '1.5.0 1.5.1'.split(' ');
+	var new_versions = '1.5.0 1.5.1 1.6.0 1.6.1'.split(' ');
 	var i;
 
 	for (i = 0; i < old_versions.length; i++) {
