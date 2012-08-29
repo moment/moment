f48812f2a29298239d686b46b069c294865794f3
Tim Wood <washwithcare@gmail.com>
2012-08-28 18:48:00 -0700

    Working on minification grunt tasks

--- grunt.js
+++ grunt.js
@@ -1,6 +1,30 @@
+var fs = require('fs');
+
 module.exports = function(grunt) {
 
+    var min = {
+        langs: {
+            src: ['min/lang-all.js'],
+            dest: 'min/lang-all.min.js'
+        }
+    };
+
+    // all the lang files need to be added manually
+    fs.readdirSync('./lang').forEach(function (path) {
+        min[path] = {
+            src: ['lang/' + path],
+            dest: 'min/lang/' + path
+        };
+    });
+
     grunt.initConfig({
+        concat : {
+            langs: {
+                src: ['lang/*.js'],
+                dest: 'min/lang-all.js'
+            }
+        },
+        min : min,
         test : {
             files : ["test/**/*.js"]
         },
@@ -29,4 +53,5 @@ module.exports = function(grunt) {
 
     // Default task.
     grunt.registerTask('default', 'test');
+    grunt.registerTask('release', 'lint test concat ');
 };
