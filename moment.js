da18f45495fddcea952a96b0f63a495c609c631a
aleafs <zhangxc83@gmail.com>
2012-11-02 20:43:31 +0800

    add space after ","

--- moment.js
+++ moment.js
@@ -126,7 +126,7 @@
             },
             w    : function () {
                 var a = new Date(this.year(), this.month(), this.date());
-                a.setHours(0,0,0);
+                a.setHours(0, 0, 0);
                 a.setDate(a.getDate() + 4 - (a.getDay() || 7));
                 return Math.ceil((1 + (a - (new Date(a.getFullYear(), 0, 1))) / 864e5) / 7);
             },
