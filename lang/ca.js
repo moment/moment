ebc081421b9f076a9274d1f66a9b991686e98308
Tim Wood <washwithcare@gmail.com>
2012-11-07 17:47:02 -0800

    ca week tests

--- lang/ca.js
+++ lang/ca.js
@@ -50,6 +50,10 @@
             },
             ordinal : function (number) {
                 return 'º';
+            },
+            week : {
+                dow : 1, // Monday is the first day of the week.
+                doy : 4  // The week that contains Jan 4th is the first week of the year.
             }
         };
 
