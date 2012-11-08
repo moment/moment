c3c0895da8b098a45a3884d5318d6d8faa77fce3
Tim Wood <washwithcare@gmail.com>
2012-11-07 17:26:44 -0800

    is week tests

--- lang/is.js
+++ lang/is.js
@@ -104,10 +104,14 @@
                 M : translate,
                 MM : translate,
                 y : translate,
-                yy : translate,
+                yy : translate
             },
             ordinal : function (number) {
                 return '.';
+            },
+            week : {
+                dow : 1, // Monday is the first day of the week.
+                doy : 4  // The week that contains Jan 4th is the first week of the year.
             }
         };
 
