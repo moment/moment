5e1153b90e8541e61e71fc3d98d72702e490458c
Peter Marsh <pete@modeltwozero.com>
2012-12-11 18:08:49 +0000

    Change from round(...) to Math.floor(...) when calculating relative number of months

--- moment.js
+++ moment.js
@@ -844,7 +844,7 @@
                 days === 1 && ['d'] ||
                 days <= 25 && ['dd', days] ||
                 days <= 45 && ['M'] ||
-                days < 345 && ['MM', round(days / 30)] ||
+                days < 345 && ['MM', Math.floor(days / 30)] ||
                 years === 1 && ['y'] || ['yy', years];
         args[2] = withoutSuffix;
         args[3] = milliseconds > 0;
