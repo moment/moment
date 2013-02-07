851f61a03a77e2605bb649aec0ea6a1ebb3a2820
Francis Besset <francis.besset@gmail.com>
2013-02-07 09:48:09 +0100

    Enhancement for french translation related to year
    It sounds more natural to say `il y a un an` than `il y a une année` and same for `dans un an` than `dans une année`.

--- lang/fr.js
+++ lang/fr.js
@@ -35,7 +35,7 @@ require('../moment').lang('fr', {
         dd : "%d jours",
         M : "un mois",
         MM : "%d mois",
-        y : "une année",
+        y : "un an",
         yy : "%d années"
     },
     ordinal : function (number) {
