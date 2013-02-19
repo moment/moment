8628fb2bebe4f942280b84803b7692d59e39b744
Igor Lima <igor.lima@evolut.io>
2013-02-19 15:29:38 -0300

    add the grunt-contrib-uglify plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,6 +100,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task.
     grunt.registerTask('default', ['jshint', 'nodeunit']);
