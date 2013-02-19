6c844788370cb425402afba79b19775d08cb4abb
Igor Lima <igor.lima@evolut.io>
2013-02-19 17:01:28 -0300

    Add concatlang in release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -106,5 +106,5 @@ module.exports = function (grunt) {
     grunt.registerTask('default', ['jshint', 'nodeunit']);
 
     // Task to be run when releasing a new version
-    grunt.registerTask('release', ['jshint', 'nodeunit', 'concat', 'uglify']);
+    grunt.registerTask('release', ['jshint', 'nodeunit', 'concatlang', 'uglify']);
 };
