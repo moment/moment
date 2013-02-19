b4d66c6f09b3210534bdb470edfb790cc8206cde
Weldan Jamili <mweldan@gmail.com>
2013-02-20 06:47:20 +0800

    update language code

--- lang/ms-my.js
+++ lang/ms-my.js
@@ -2,7 +2,7 @@
 // language : Bahasa Malaysia (ms-MY)
 // author : Weldan Jamili : https://github.com/weldan
 
-require('../moment').lang('ms', {
+require('../moment').lang('ms-my', {
     months : "Januari_Februari_Mac_April_Mei_Jun_Julai_Ogos_September_Oktober_November_Disember".split("_"),
     monthsShort : "Jan_Feb_Mac_Apr_Mei_Jun_Jul_Ogs_Sep_Okt_Nov_Dis".split("_"),
     weekdays : "Minggu_Isnin_Selasa_Rabu_Khamis_Jumaat_Sabtu".split("_"),
