7564a026cf990b7948329ac93bb5712be2b1d4ec
mechuwind <martinjarrettno@gmail.com>
2013-04-25 12:10:22 +0300

    Update nn.js

--- lang/nn.js
+++ lang/nn.js
@@ -2,7 +2,7 @@
 // language : norwegian nynorsk (nn)
 // author : https://github.com/mechuwind
 
-require('../moment').lang('nb', {
+require('../moment').lang('nn', {
     months : "januar_februar_mars_april_mai_juni_juli_august_september_oktober_november_desember".split("_"),
     monthsShort : "jan_feb_mar_apr_mai_jun_jul_aug_sep_okt_nov_des".split("_"),
     weekdays : "sundag_måndag_tysdag_onsdag_torsdag_fredag_laurdag".split("_"),
