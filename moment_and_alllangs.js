216443fc8c36a6811824718bc58ba3ca86c44965
Brian Wyant <wyantb@gmail.com>
2013-08-01 19:30:01 -0400

    Recompile with languages in main moment closure

--- moment_and_alllangs.js
+++ moment_and_alllangs.js
@@ -1697,33 +1697,7 @@
         }
     });
 
-
-    /************************************
-        Exposing Moment
-    ************************************/
-
-
-    // CommonJS module is defined
-    if (hasModule) {
-        module.exports = moment;
-    }
-    /*global ender:false */
-    if (typeof ender === 'undefined') {
-        // here, `this` means `window` in the browser, or `global` on the server
-        // add `moment` as a global object via a string identifier,
-        // for Closure Compiler "advanced" mode
-        this['moment'] = moment;
-    }
-    /*global define:false */
-    if (typeof define === "function" && define.amd) {
-        define("moment", [], function () {
-            return moment;
-        });
-    }
-}).call(this);
-
-(function(){
-    function onload (moment) {
+    (function(){
 (function(){
 // moment.js language configuration
 // language : Moroccan Arabic (ar-ma)
@@ -5406,9 +5380,29 @@ moment.lang('zh-tw', {
 })();
 
 moment.lang('en');
-
-    }
-    if (typeof window !== "undefined" && window.moment) {
-        onload(window.moment);
-    }
 })();
+
+
+    /************************************
+        Exposing Moment
+    ************************************/
+
+
+    // CommonJS module is defined
+    if (hasModule) {
+        module.exports = moment;
+    }
+    /*global ender:false */
+    if (typeof ender === 'undefined') {
+        // here, `this` means `window` in the browser, or `global` on the server
+        // add `moment` as a global object via a string identifier,
+        // for Closure Compiler "advanced" mode
+        this['moment'] = moment;
+    }
+    /*global define:false */
+    if (typeof define === "function" && define.amd) {
+        define("moment", [], function () {
+            return moment;
+        });
+    }
+}).call(this);
