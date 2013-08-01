22338fbea79553fc51b30a0ebb84a6c1b91bd832
Brian Wyant <wyantb@gmail.com>
2013-08-01 18:47:02 -0400

    Update concatwithlang; put langs in moment closure

--- tasks/concat-with-lang.js
+++ tasks/concat-with-lang.js
@@ -5,31 +5,21 @@ module.exports = function (grunt) {
     var helpers = require('grunt-lib-legacyhelpers').init(grunt);
     var START = "(function(){\n";
     var END   = "})();\n";
-
-    var GLOBAL_START = [
-        "(function(){",
-        "    function onload (moment) {",
-        ""
-    ].join('\n');
-
-    var GLOBAL_RESET = "\nmoment.lang('en');\n";
-
-    var GLOBAL_END = [
-        "",
-        "    }",
-        "    if (typeof window !== \"undefined\" && window.moment) {",
-        "        onload(window.moment);",
-        "    }",
-        "})();",
-        ""
-    ].join('\n');
+    var RESET = "\nmoment.lang('en');\n";
 
     grunt.registerMultiTask('concatwithlang', 'Concatenate files.', function() {
         var momentFile = grunt.file.read(this.data.moment);
 
         var files = grunt.file.expand(this.data.src);
         var src = helpers.concat(files, {separator: END + START});
-        grunt.file.write(this.data.dest, momentFile + '\n' + wrapFile(src));
+        var languages = wrapFile(src);
+
+        var outputFile = momentFile.replace('/* EMBED_LANGUAGES */', function () {
+            // If we don't do this, $ symbols in lang files may get interpreted in
+            //  the regex replacement
+            return languages;
+        });
+        grunt.file.write(this.data.dest, outputFile);
 
         // Fail task if errors were logged.
         if (this.errorCount) { return false; }
@@ -40,6 +30,6 @@ module.exports = function (grunt) {
 
     function wrapFile(code) {
         code = code.replace(/require\([\'\"]\.\.\/moment[\'\"]\)/g, "moment");
-        return GLOBAL_START + START + code + END + GLOBAL_RESET + GLOBAL_END;
+        return START + START + code + END + RESET + END;
     }
 };
