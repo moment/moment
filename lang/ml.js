a518f8dd06c28811075d02150036cad94cca1605
Floyd Pink <imbleedingme@googlemail.com>
2013-08-15 17:27:33 +0000

    deault dow and doy are just fine

--- lang/ml.js
+++ lang/ml.js
@@ -50,9 +50,5 @@ require('../moment').lang('ml', {
         } else {
             return "രാത്രി";
         }
-    },
-    week : {
-        dow : 1, // Monday is the first day of the week.
-        doy : 4  // The week that contains Jan 4th is the first week of the year.
     }
 });
