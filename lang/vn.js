22874b1faeb90f3e933433292203480481cca961
bangnk <nkbang@gmail.com>
2013-09-09 22:24:46 +0700

    Add test file for vietnamese, fix some constants to be more meaningful in vietnamese

--- lang/vn.js
+++ lang/vn.js
@@ -3,7 +3,7 @@
 // author : Bang Nguyen : https://github.com/bangnk
 
 require('../moment').lang('vn', {
-    months : "tháng một_tháng hai_tháng ba_tháng tư_tháng năm_tháng sáu_tháng bảy_tháng tám_tháng chín_tháng mười_tháng mười một_tháng mười hai".split("_"),
+    months : "tháng 1_tháng 2_tháng 3_tháng 4_tháng 5_tháng 6_tháng 7_tháng 8_tháng 9_tháng 10_tháng 11_tháng 12".split("_"),
     monthsShort : "Th01_Th02_Th03_Th04_Th05_Th06_Th07_Th08_Th09_Th10_Th11_Th12".split("_"),
     weekdays : "chủ nhật_thứ hai_thứ ba_thứ tư_thứ năm_thứ sáu_thứ bảy".split("_"),
     weekdaysShort : "CN_T2_T3_T4_T5_T6_T7".split("_"),
@@ -11,16 +11,20 @@ require('../moment').lang('vn', {
     longDateFormat : {
         LT : "HH:mm",
         L : "DD/MM/YYYY",
-        LL : "D MMMM YYYY",
-        LLL : "D MMMM YYYY LT",
-        LLLL : "dddd D MMMM YYYY LT"
+        LL : "D MMMM [năm] YYYY",
+        LLL : "D MMMM [năm] YYYY LT",
+        LLLL : "dddd, D MMMM [năm] YYYY LT",
+        l : "DD/M/YYYY",
+        ll : "D MMM YYYY",
+        lll : "D MMM YYYY LT",
+        llll : "ddd, D MMM YYYY LT"
     },
     calendar : {
         sameDay: "[Hôm nay lúc] LT",
         nextDay: '[Ngày mai lúc] LT',
-        nextWeek: 'dddd [tới lúc] LT',
+        nextWeek: 'dddd [tuần tới lúc] LT',
         lastDay: '[Hôm qua lúc] LT',
-        lastWeek: 'dddd [rồi lúc] LT',
+        lastWeek: 'dddd [tuần rồi lúc] LT',
         sameElse: 'L'
     },
     relativeTime : {
