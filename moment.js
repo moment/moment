7878e30ca0631f4d279510ce435c800a8ce56b1d
Xotic750 <xotic750@gmail.com>
2013-09-13 01:48:09 +0200

    Suggested change to solve problem with globalisation of moment when using RequireJS Test uith RequireJS: http://jsfiddle.net/Xotic750/xA2JL/ Test with script tag: http://jsfiddle.net/Xotic750/R3EVu/ No test available for CommonJS https://github.com/moment/moment/issues/1064

--- moment.js
+++ moment.js
@@ -1759,22 +1759,29 @@
         Exposing Moment
     ************************************/
 
+    function makeGlobal () {
+        /*global ender:false */
+        if (typeof ender === 'undefined') {
+            // here, `this` means `window` in the browser, or `global` on the server
+            // add `moment` as a global object via a string identifier,
+            // for Closure Compiler "advanced" mode
+            this['moment'] = moment;
+        }
+    }
 
     // CommonJS module is defined
     if (hasModule) {
         module.exports = moment;
-    }
-    /*global ender:false */
-    if (typeof ender === 'undefined') {
-        // here, `this` means `window` in the browser, or `global` on the server
-        // add `moment` as a global object via a string identifier,
-        // for Closure Compiler "advanced" mode
-        this['moment'] = moment;
-    }
-    /*global define:false */
-    if (typeof define === "function" && define.amd) {
-        define("moment", [], function () {
+        makeGlobal();
+    } else if (typeof define === "function" && define.amd) {
+        define("moment", function (require, exports, module) {
+            if (module.config().noGlobal !== true) {
+                makeGlobal();
+            }
+
             return moment;
         });
+    } else {
+        makeGlobal();
     }
 }).call(this);
