5b7f02dded73853faf1b1eac17dd1419ca5a1620
Xotic750 <xotic750@gmail.com>
2013-09-13 01:40:41 +0200

    Added tests for moment.normalizeUnits https://github.com/moment/moment/issues/1066

---
+++ test/moment/normalizeUnits.js
@@ -0,0 +1,34 @@
+var moment = require("../../moment");
+
+exports.normalizeUnits = {
+    "normalize units" : function (test) {
+        test.expect(54);
+        var fullKeys = "year_month_isoweek_week_day_hour_minute_second_milliseconds".split("_"),
+            aliases = "y_m_W_w_d_h_m_s_ms".split("_"),
+            length = full.length,
+            fullKey,
+            fullKeyCaps
+            fullKeyPlural,
+            fullKeyCapsPlural,
+            alias,
+            aliasCaps,
+            index;
+
+        for (index = 0; index < length; index += 1) {
+            fullKey = fullkeys[index];
+            fullKeyCaps = fullKey.toUpperCase();
+            fullKeyPlural = fullKey + "s";
+            fullKeyCapsPlural = fullKeyCaps + "s",
+            alias = aliases[index];
+            aliasCaps = alias.toUpperCase
+            test.equal(moment.normalizeUnits(fullKey), fullKey, '2010');
+            test.equal(moment.normalizeUnits(fullKeyCaps), fullKey, '2010');
+            test.equal(moment.normalizeUnits(fullKeyPlural), fullKey, '2010');
+            test.equal(moment.normalizeUnits(fullKeyCapsPlural), fullKey, '2010');
+            test.equal(moment.normalizeUnits(alias), fullKey, '2010');
+            test.equal(moment.normalizeUnits(aliasCaps), fullKey, '2010');
+        }
+
+        test.done();
+    }
+};
