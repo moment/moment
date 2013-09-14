b4b29cb6a55ecd686897f97f3c2f9fb172615f79
Xotic750 <xotic750@gmail.com>
2013-09-15 01:04:36 +0200

    Added feature to expose normalizeUnits

--- moment.js
+++ moment.js
@@ -1215,6 +1215,11 @@
         return obj instanceof Duration;
     };
 
+    // for use by developers when extending the library
+    // https://github.com/moment/moment/issues/1066
+    moment.normalizeUnits = function (units) {
+        return normalizeUnits(units);
+    };
 
     /************************************
         Moment Prototype
