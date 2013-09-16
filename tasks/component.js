e5784d35a3a4d27f1e51080f72208f8897294d31
Iskren Chernev <iskren.chernev@gmail.com>
2013-09-16 03:38:08 -0700

    Added component grunt task to fill component.json with lang files

---
+++ tasks/component.js
@@ -0,0 +1,9 @@
+module.exports = function (grunt) {
+    grunt.registerTask('component', function () {
+        var config = JSON.parse(grunt.file.read('component.json'));
+        config.files = grunt.file.expand('lang/*.js');
+        config.files.unshift('moment.js');
+
+        grunt.file.write('component.json', JSON.stringify(config, true, 2));
+    });
+}
