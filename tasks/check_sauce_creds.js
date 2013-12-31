32c1c3448a2a42eec619ae918dea9eedfdec91f9
Iskren Chernev <iskren.chernev@gmail.com>
2013-12-31 17:50:34 -0300

    PRs now pass travis build, because they don't execute sauce tests

---
+++ tasks/check_sauce_creds.js
@@ -0,0 +1,14 @@
+module.exports = function (grunt) {
+
+    // Pull Requests do not have secure variables enabled for security reasons.
+    // Use this task before launching travis-sauce-browser task, so it would
+    // exit early and won't try connecting to saucelabs without credentials.
+    grunt.registerTask('check-sauce-creds', function () {
+        if (process.env.SAUCE_USERNAME === undefined) {
+            grunt.log.writeln("No sauce credentials found");
+            grunt.task.clearQueue();
+        } else {
+            grunt.log.writeln("Sauce credentials found");
+        }
+    });
+};
