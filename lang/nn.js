6c373dc68be7880b59e26bdada690697654dd73d
Iskren Chernev <iskren.chernev@gmail.com>
2014-03-12 00:32:29 -0700

    Fix short date format in nn according to 39693d28c12cd

--- lang/nn.js
+++ lang/nn.js
@@ -19,7 +19,7 @@
         weekdaysMin : "su_må_ty_on_to_fr_lø".split("_"),
         longDateFormat : {
             LT : "HH:mm",
-            L : "YYYY-MM-DD",
+            L : "DD.MM.YYYY",
             LL : "D MMMM YYYY",
             LLL : "D MMMM YYYY LT",
             LLLL : "dddd D MMMM YYYY LT"
