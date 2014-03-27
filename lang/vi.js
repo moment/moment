101c726c77c3dd119bd5e7fbfa7d5c226d4b6ca3
Minh Nguyễn <mxn@1ec5.org>
2014-03-27 02:24:42 -0700

    Vietnamese is vi, not vn

--- lang/vi.js
+++ lang/vi.js
@@ -1,5 +1,5 @@
 // moment.js language configuration
-// language : vietnamese (vn)
+// language : vietnamese (vi)
 // author : Bang Nguyen : https://github.com/bangnk
 
 (function (factory) {
@@ -11,7 +11,7 @@
         factory(window.moment); // Browser global
     }
 }(function (moment) {
-    return moment.lang('vn', {
+    return moment.lang('vi', {
         months : "tháng 1_tháng 2_tháng 3_tháng 4_tháng 5_tháng 6_tháng 7_tháng 8_tháng 9_tháng 10_tháng 11_tháng 12".split("_"),
         monthsShort : "Th01_Th02_Th03_Th04_Th05_Th06_Th07_Th08_Th09_Th10_Th11_Th12".split("_"),
         weekdays : "chủ nhật_thứ hai_thứ ba_thứ tư_thứ năm_thứ sáu_thứ bảy".split("_"),
