268c34679b60c07b4c929f13928db93eca303299
Iskren Chernev <iskren.chernev@gmail.com>
2014-05-01 10:39:07 -0700

    Tests for moment.min/max

--- test/moment/min_max.js
+++ test/moment/min_max.js
@@ -1,9 +1,5 @@
 var moment = require("../../moment");
 
-var equalMoment = function (test, a, b, msg) {
-    test.equal(a.valueOf(), b.valueOf(), msg);
-};
-
 exports.minMax = {
     setUp : function (cb) {
         moment.lang('en');
@@ -19,39 +15,41 @@ exports.minMax = {
     },
 
     "min" : function (test) {
-        test.expect(6);
-
         var now = moment(),
             future = now.clone().add(1, 'month'),
             past = now.clone().subtract(1, 'month');
 
-        equalMoment(test, past.min(now), now,       "A past date with the minimum of now should be now");
-        equalMoment(test, past.min(future), future, "A past date with the minimum of the future should be the future date");
+        test.equal(moment.min(now, future, past), past, "min(now, future, past)");
+        test.equal(moment.min(future, now, past), past, "min(future, now, past)");
+        test.equal(moment.min(future, past, now), past, "min(future, past, now)");
+        test.equal(moment.min(past, future, now), past, "min(past, future, now)");
+        test.equal(moment.min(now, past), past, "min(now, past)");
+        test.equal(moment.min(past, now), past, "min(past, now)");
+        test.equal(moment.min(now), now, "min(now, past)");
 
-        equalMoment(test, future.min(now), future,  "A future date with the minimum of now should be the future");
-        equalMoment(test, future.min(past), future, "A future date with the minimum of the past should be the future");
-
-        equalMoment(test, now.min(past), now,       "Now with the minimum of the past should be now");
-        equalMoment(test, now.min(future), future,  "Now with the minimum of the future should be the future");
+        test.equal(moment.min([now, future, past]), past, "min([now, future, past])");
+        test.equal(moment.min([now, past]), past, "min(now, past)");
+        test.equal(moment.min([now]), now, "min(now)");
 
         test.done();
     },
 
     "max" : function (test) {
-        test.expect(6);
-
         var now = moment(),
             future = now.clone().add(1, 'month'),
             past = now.clone().subtract(1, 'month');
 
-        equalMoment(test, past.max(now), past,    "A past date with the maximum of now should be the past");
-        equalMoment(test, past.max(future), past, "A past date with the maximum of the future should be the past");
+        test.equal(moment.max(now, future, past), future, "max(now, future, past)");
+        test.equal(moment.max(future, now, past), future, "max(future, now, past)");
+        test.equal(moment.max(future, past, now), future, "max(future, past, now)");
+        test.equal(moment.max(past, future, now), future, "max(past, future, now)");
+        test.equal(moment.max(now, past), now, "max(now, past)");
+        test.equal(moment.max(past, now), now, "max(past, now)");
+        test.equal(moment.max(now), now, "max(now, past)");
 
-        equalMoment(test, future.max(now), now,   "A future date with the maximum of now should be now");
-        equalMoment(test, future.max(past), past, "A future date with the maximum of the past should be the past");
-
-        equalMoment(test, now.max(past), past,    "Now with the maximum of the past should be the past");
-        equalMoment(test, now.max(future), now,   "Now with the maximum of the future should be now");
+        test.equal(moment.max([now, future, past]), future, "max([now, future, past])");
+        test.equal(moment.max([now, past]), now, "max(now, past)");
+        test.equal(moment.max([now]), now, "max(now)");
 
         test.done();
     }
