41629709a51b19c796188a52976920a3c82bff88
David Rossellat <david.rossellat@gmail.com>
2014-05-30 15:21:07 +0700

    adding MM

---
+++ lang/mm.js
@@ -0,0 +1,55 @@
+// moment.js language configuration
+// language : Burmese (mm)
+// author : Squar team, mysquar.com
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['moment'], factory); // AMD
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('../moment')); // Node
+    } else {
+        factory(window.moment); // Browser global
+    }
+}(function (moment) {
+    return moment.lang('mm', {
+        months : " ဇန္နဝါရီ_ ေဖေဖာ္ဝါရီ_ မတ္_ ဧျပီ_ ေမ_ ဇြန္_ဇူလိုင္_ ၾသဂုတ္_ စက္တင္ဘာ_ ေအာက္တိုဘာ_ ႏိုဝင္ဘာ_ ဒီဇင္ဘာ".split("_"),
+        monthsShort : "ဇန္_ေဖ_မတ္_ျပီ_ေမ_ဇြန္_ဂ်ဴလိုင္_ၾသ_စက္_ေအာက္_ႏို_ဒီ".split("_"),
+        weekdays : "တနလာၤေန႔_အဂၤါေန႔_ဗုဒၶဟူးေန႔_ၾကာသပေတးေန႔_ေသာၾကာေန႔_စေနေန႔_တနဂၤေႏြေန႔".split("_"),
+        weekdaysShort : "လာ_ဂၤါ_ဟူး_ၾကာ_ေသာ_စ_ေႏြြ".split("_"),
+        weekdaysMin : "လာ_ဂၤါ_ဟူး_ၾကာ_ေသာ_စ_ေႏြ".split("_"),
+        longDateFormat : {
+            LT : "HH.mm",
+            L : "DD/MM/YYYY",
+            LL : "D MMMM YYYY",
+            LLL : "D MMMM YYYY [pukul] LT",
+            LLLL : "dddd, D MMMM YYYY [pukul] LT"
+        },
+        calendar : {
+            sameDay : '[ယေန႔] LT [မွာ]',
+            nextDay : '[မနက္ျဖန္] LT [မွာ]',
+            nextWeek : 'dddd [မွ] LT',
+            lastDay : '[မေန႔က] LT [မွာ]',
+            lastWeek : '[ေနာက္ဆံုး] dddd LT',
+            sameElse : 'L'
+        },
+        relativeTime : {
+            future : "%s မွာ",
+            past : "လြန္ခဲ့ေသာ %s က",
+            s : "စကၠန္႔အနည္းငယ္",
+            m : "တစ္မိနစ္",
+            mm : "%d မိနစ္",
+            h : "တစ္နာရီ",
+            hh : "%d နာရီ",
+            d : "တစ္ေန႔ ",
+            dd : "%d ရက္",
+            M : "တစ္လ",
+            MM : "%d လ",
+            y : "တစ္ႏွစ္",
+            yy : "%d ႏွစ္"
+        },
+        week : {
+            dow : 1, // Monday is the first day of the week.
+            doy : 7  // The week that contains Jan 1st is the first week of the year.
+        }
+    });
+}));
