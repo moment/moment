c95e34069cfe6fa5a74bae4a6417b56ce02a2f4a
Iskren Chernev <iskren.chernev@gmail.com>
2014-06-11 23:59:31 -0700

    Bump version to 2.7.0

--- moment.js
+++ moment.js
@@ -1,5 +1,5 @@
 //! moment.js
-//! version : 2.6.0
+//! version : 2.7.0
 //! authors : Tim Wood, Iskren Chernev, Moment.js contributors
 //! license : MIT
 //! momentjs.com
@@ -11,7 +11,7 @@
     ************************************/
 
     var moment,
-        VERSION = "2.6.0",
+        VERSION = "2.7.0",
         // the global-scope this is NOT the global object in Node.js
         globalScope = typeof global !== 'undefined' ? global : this,
         oldGlobalMoment,
