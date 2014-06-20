e1e4d54c7063c3f43cd21e842bf447a37bd188bf
Anton Wilhelm <timaschew@gmail.com>
2014-06-20 14:23:13 +0200

    fix #1730 use .scripts instead of .files

--- tasks/component.js
+++ tasks/component.js
@@ -2,9 +2,9 @@ module.exports = function (grunt) {
     grunt.registerTask('component', function () {
         var config = JSON.parse(grunt.file.read('component.json'));
 
-        config.files = grunt.file.expand('lang/*.js');
-        config.files.unshift('moment.js');
+        config.scripts = grunt.file.expand('lang/*.js');
+        config.scripts.unshift('moment.js');
 
         grunt.file.write('component.json', JSON.stringify(config, true, 2));
     });
-}
+};
