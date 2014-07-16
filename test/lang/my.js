14ba701555f15c0fcd550fa0709f295a512eac5b
htlwin2006 <htlwin2006@users.noreply.github.com>
2014-07-16 17:01:23 +0630

    Update my.js

--- test/lang/my.js
+++ test/lang/my.js
@@ -114,16 +114,16 @@ exports["lang:my"] = {
 
     "format month" : function (test) {
         test.expect(12);
-        var expected = 'ဇန္နဝါရီ ဇန္_ေဖေဖာ္ဝါရီ ေဖ_မတ္ မတ္_ဧျပီ ျပီ_ေမ ေမ_ဇြန္ ဇြန္_ဇူလိုင္ လိုင္_ၾသဂုတ္ ၾသ_စက္တင္ဘာ စက္_ေအာက္တိုဘာ ေအာက္_ႏိုဝင္ဘာ ႏို_ဒီဇင္ဘာ ဒီ'.split("_"),i;
+        var expected = 'ဇန်နဝါရီ ဇန်_ဖေဖော်ဝါရီ ဖေ_မတ် မတ်_ဧပြီ ပြီ_မေ မေ_ဇွန် ဇွန်_ဇူလိုင် လိုင်_သြဂုတ် သြ_စက်တင်ဘာ စက်_အောက်တိုဘာ အောက်_နိုဝင်ဘာ နို_ဒီဇင်ဘာ ဒီ'.split("_"),i;
         for (i = 0; i < expected.length; i++) {
             test.equal(moment([2011, i, 1]).format('MMMM MMM'), expected[i], expected[i]);
         }
         test.done();
     },
-/*
+
     "format week" : function (test) {
         test.expect(7);
-        var expected = 'တနဂၤေႏြ ေႏြ ေႏြ_တနလာၤ လာ လာ_အဂၤါ ဂါ ဂါ_ဗုဒၶဟူးေန႔ ဟူး ဟူး_ၾကာသပေတး ေတး ေတး_ေသာၾကာ ေသာ ေသာ_စေန ေန ေန'.split("_"), i;
+        var expected = 'တနင်္ဂနွေ နွေ နွေ_တနင်္လာ လာ လာ_အင်္ဂါ င်္ဂါ င်္ဂါ_ဗုဒ္ဓဟူး ဟူး ဟူး_ကြာသပတေး ကြာ ကြာ_သောကြာ သော သော_စနေ နေ နေ'.split("_"), i;
 
         for (i = 0; i < expected.length; i++) {
             test.equal(moment([2011, 0, 2 + i]).format('dddd ddd dd'), expected[i], expected[i]);
@@ -134,56 +134,56 @@ exports["lang:my"] = {
     "from" : function (test) {
         test.expect(30);
         var start = moment([2007, 1, 28]);
-     test.equal(start.from(moment([2007, 1, 28]).add({s: 44}), true),  "စကၠန္႔အနည္းငယ္က", "၄၄ = စကၠန္႔အနည္းငယ္က");
-        test.equal(start.from(moment([2007, 1, 28]).add({s: 45}), true),  "တစ္မိနစ္",      "၄၅ စကၠန္႔ = တစ္မိနစ္");
-        test.equal(start.from(moment([2007, 1, 28]).add({s: 89}), true),  "တစ္မိနစ္",      "၈၉ စကၠန္႔ = တစ္မိနစ္");
-        test.equal(start.from(moment([2007, 1, 28]).add({s: 90}), true),  "၂ မိနစ္",     "၉၀ စကၠန္႔ =  ၂ မိနစ္");
-        test.equal(start.from(moment([2007, 1, 28]).add({m: 44}), true),  "၄၄ မိနစ္",    "၄၄ မိနစ္ = ၄၄ မိနစ္");
-        test.equal(start.from(moment([2007, 1, 28]).add({m: 45}), true),  "တစ္နာရီ",       "၄၅ မိနစ္ = ၁ နာရီ");
-        test.equal(start.from(moment([2007, 1, 28]).add({m: 89}), true),  "တစ္နာရီ",       "၈၉ မိနစ္ = တစ္နာရီ");
-        test.equal(start.from(moment([2007, 1, 28]).add({m: 90}), true),  "၂ နာရီ",       "မိနစ္ ၉၀= ၂ နာရီ");
+     	test.equal(start.from(moment([2007, 1, 28]).add({s: 44}), true),  "စက္ကန်.အနည်းငယ်", "၄၄ စက္ကန်. = စက္ကန်.အနည်းငယ်");
+        test.equal(start.from(moment([2007, 1, 28]).add({s: 45}), true),  "တစ်မိနစ်",      "၄၅ စက္ကန်. = တစ်မိနစ်");
+        test.equal(start.from(moment([2007, 1, 28]).add({s: 89}), true),  "တစ်မိနစ်",      "၈၉ စက္ကန်. = တစ်မိနစ်");
+        test.equal(start.from(moment([2007, 1, 28]).add({s: 90}), true),  "၂ မိနစ်",     "၉၀ စက္ကန်. =  ၂ မိနစ်");
+        test.equal(start.from(moment([2007, 1, 28]).add({m: 44}), true),  "၄၄ မိနစ်",    "၄၄ မိနစ် = ၄၄ မိနစ်");
+        test.equal(start.from(moment([2007, 1, 28]).add({m: 45}), true),  "တစ်နာရီ",       "၄၅ မိနစ် = ၁ နာရီ");
+        test.equal(start.from(moment([2007, 1, 28]).add({m: 89}), true),  "တစ်နာရီ",       "၈၉ မိနစ် = တစ်နာရီ");
+        test.equal(start.from(moment([2007, 1, 28]).add({m: 90}), true),  "၂ နာရီ",       "မိနစ် ၉၀= ၂ နာရီ");
         test.equal(start.from(moment([2007, 1, 28]).add({h: 5}), true),   "၅ နာရီ",       "၅ နာရီ= ၅ နာရီ");
         test.equal(start.from(moment([2007, 1, 28]).add({h: 21}), true),  "၂၁ နာရီ",      "၂၁ နာရီ =၂၁ နာရီ");
-        test.equal(start.from(moment([2007, 1, 28]).add({h: 22}), true),  "တစ္ရက္",         "၂၂ နာရီ =တစ္ရက္");
-        test.equal(start.from(moment([2007, 1, 28]).add({h: 35}), true),  "တစ္ရက္",         "၃၅ နာရီ =တစ္ရက္");
-        test.equal(start.from(moment([2007, 1, 28]).add({h: 36}), true),  "၂ ရက္",        "၃၆ နာရီ = ၂ ရက္");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 1}), true),   "တစ္ရက္",         "၁ ရက္= တစ္ရက္");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 5}), true),   "၅ ရက္",        "၅ ရက္ = ၅ ရက္");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 25}), true),  "၂၅ ရက္",       "၂၅ ရက္= ၂၅ ရက္");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 26}), true),  "တစ္လ",       "၂၆ ရက္ = တစ္လ");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 30}), true),  "တစ္လ",       "ရက္ ၃၀ = တစ္လ");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 45}), true),  "တစ္လ",       "၄၅ ရက္ = တစ္လ");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 46}), true),  "၂ လ",      "၄၆ ရက္ = ၂ လ");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 74}), true),  "၂ လ",      "၇၅ ရက္= ၂ လ");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 76}), true),  "၃ လ",      "၇၆ ရက္ = ၃ လ");
-        test.equal(start.from(moment([2007, 1, 28]).add({M: 1}), true),   "တစ္လ",       "၁ လ = တစ္လ");
+        test.equal(start.from(moment([2007, 1, 28]).add({h: 22}), true),  "တစ်ရက်",         "၂၂ နာရီ =တစ်ရက်");
+        test.equal(start.from(moment([2007, 1, 28]).add({h: 35}), true),  "တစ်ရက်",         "၃၅ နာရီ =တစ်ရက်");
+        test.equal(start.from(moment([2007, 1, 28]).add({h: 36}), true),  "၂ ရက်",        "၃၆ နာရီ = ၂ ရက်");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 1}), true),   "တစ်ရက်",         "၁ ရက်= တစ်ရက်");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 5}), true),   "၅ ရက်",        "၅ ရက် = ၅ ရက်");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 25}), true),  "၂၅ ရက်",       "၂၅ ရက်= ၂၅ ရက်");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 26}), true),  "တစ်လ",       "၂၆ ရက် = တစ်လ");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 30}), true),  "တစ်လ",       "ရက် ၃၀ = တစ်လ");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 45}), true),  "တစ်လ",       "၄၅ ရက် = တစ်လ");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 46}), true),  "၂ လ",      "၄၆ ရက် = ၂ လ");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 74}), true),  "၂ လ",      "၇၅ ရက်= ၂ လ");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 76}), true),  "၃ လ",      "၇၆ ရက် = ၃ လ");
+        test.equal(start.from(moment([2007, 1, 28]).add({M: 1}), true),   "တစ်လ",       "၁ လ = တစ်လ");
         test.equal(start.from(moment([2007, 1, 28]).add({M: 5}), true),   "၅ လ",      "၅ လ = ၅ လ");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 344}), true), "၁၁ လ",     "၃၄၄ ရက္ = ၁၁ လ");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 345}), true), "တစ္ႏွစ္",        "၃၄၅ ရက္ = တစ္ႏွစ္");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 547}), true), "တစ္ႏွစ္",        "၅၇၄ ရက္ = တစ္ႏွစ္");
-        test.equal(start.from(moment([2007, 1, 28]).add({d: 548}), true), "၂ ႏွစ္",       "၅၄၈ ရက္ = ၂ ႏွစ္");
-        test.equal(start.from(moment([2007, 1, 28]).add({y: 1}), true),   "တစ္ႏွစ္",        "၁ ႏွစ္ = တစ္ႏွစ္");
-        test.equal(start.from(moment([2007, 1, 28]).add({y: 5}), true),   "၅ ႏွစ္",       "၅ ႏွစ္ = ၅ ႏွစ္");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 344}), true), "၁၁ လ",     "၃၄၄ ရက် = ၁၁ လ");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 345}), true), "တစ်နှစ်",        "၃၄၅ ရက် = တစ်နှစ်");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 547}), true), "တစ်နှစ်",        "၅၇၄ ရက် = တစ်နှစ်");
+        test.equal(start.from(moment([2007, 1, 28]).add({d: 548}), true), "၂ နှစ်",       "၅၄၈ ရက် = ၂ နှစ်");
+        test.equal(start.from(moment([2007, 1, 28]).add({y: 1}), true),   "တစ်နှစ်",        "၁ နှစ် = တစ်နှစ်");
+        test.equal(start.from(moment([2007, 1, 28]).add({y: 5}), true),   "၅ နှစ်",       "၅ နှစ် = ၅ နှစ်");
         test.done();
     },
 
     "suffix" : function (test) {
         test.expect(2);
-        test.equal(moment(30000).from(0), "စကၠန္႔အနည္းငယ္အတြင္း",  "prefix");
-        test.equal(moment(0).from(30000), "လြန္ခဲ့ေသာစကၠန္႔အနည္းငယ္က", "suffix");
+        test.equal(moment(30000).from(0), "လာမည့် စက္ကန်.အနည်းငယ် မှာ",  "prefix");
+        test.equal(moment(0).from(30000), "လွန်ခဲ့သော စက္ကန်.အနည်းငယ် က", "suffix");
         test.done();
     },
 
     "now from now" : function (test) {
         test.expect(1);
-        test.equal(moment().fromNow(), "လြန္ခဲ့ေသာစကၠန္႔အနည္းငယ္က",  "ယခုမွစျပီး အတိတ္တြင္ေဖာ္ျပသလိုေဖာ္ျပမည္");
+        test.equal(moment().fromNow(), "လွန်ခဲ့သော စက္ကန်.အနည်းငယ် က",  "ယခုမှစပြီး အတိတ်တွင်ဖော်ပြသလိုဖော်ပြမည်");
         test.done();
     },
 
     "fromNow" : function (test) {
         test.expect(2);
-        test.equal(moment().add({s: 30}).fromNow(), "စကၠန္႔အနည္းငယ္အတြင္း", "စကၠန္႔အနည္းငယ္အတြင္း");
-        test.equal(moment().add({d: 5}).fromNow(), "၅ ရက္တြင္", "၅ ရက္တြင္");
+        test.equal(moment().add({s: 30}).fromNow(), "လာမည့် စက္ကန်.အနည်းငယ် မှာ", "လာမည့် စက္ကန်.အနည်းငယ် မှာ");
+        test.equal(moment().add({d: 5}).fromNow(), "လာမည့် ၅ ရက် မှာ", "လာမည့် ၅ ရက် မှာ");
         test.done();
     },
 
@@ -192,12 +192,12 @@ exports["lang:my"] = {
 
         var a = moment().hours(2).minutes(0).seconds(0);
 
-        test.equal(moment(a).calendar(),                     "ယေန႔ ၀၂ : ၀၀ တြင္",      "ယေန႔ အခ်ိန္တူတူတြင္");
-        test.equal(moment(a).add({ m: 25 }).calendar(),      "ယေန႔ ၀၂ : ၂၅ တြင္",      "ယခုမွ ၂၅ မိနစ္ေပါင္းထည့္");
-        test.equal(moment(a).add({ h: 1 }).calendar(),       "ယေန႔ ၀၃ : ၀၀ တြင္",      "ယခုမွ ၁ နာရီေပါင္းထည့္");
-        test.equal(moment(a).add({ d: 1 }).calendar(),       "မနက္ျဖန္ ၀၂ : ၀၀ တြင္",   "မနက္ျဖန္ အခ်ိန္ အတူတူတြင္");
-        test.equal(moment(a).subtract({ h: 1 }).calendar(),  "ယေန႔ ၀၁ : ၀၀ တြင္",      "၁ နာရီ ယခုမိနစ္");
-        test.equal(moment(a).subtract({ d: 1 }).calendar(),  "မေန႔က ၀၂:၀၀ တြင္",  "မေန႔က အခ်ိန္အတူတူတြင္");
+        test.equal(moment(a).calendar(),                     "ယနေ. ၀၂:၀၀ မှာ",      "ယနေ. ဒီအချိန်");
+        test.equal(moment(a).add({ m: 25 }).calendar(),      "ယနေ. ၀၂:၂၅ မှာ",      "ယခုမှ ၂၅ မိနစ်ပေါင်းထည့်");
+        test.equal(moment(a).add({ h: 1 }).calendar(),       "ယနေ. ၀၃:၀၀ မှာ",      "ယခုမှ ၁ နာရီပေါင်းထည့်");
+        test.equal(moment(a).add({ d: 1 }).calendar(),       "မနက်ဖြန် ၀၂:၀၀ မှာ",   "မနက်ဖြန် ဒီအချိန်");
+        test.equal(moment(a).subtract({ h: 1 }).calendar(),  "ယနေ. ၀၁:၀၀ မှာ",      "ယခုမှ ၁ နာရီနှုတ်");
+        test.equal(moment(a).subtract({ d: 1 }).calendar(),  "မနေ.က ၀၂:၀၀ မှာ",  "မနေ.က ဒီအချိန်");
         test.done();
     },
 
@@ -207,11 +207,11 @@ exports["lang:my"] = {
         var i, m;
         for (i = 2; i < 7; i++) {
             m = moment().add({ d: i });
-            test.equal(m.calendar(),       m.format('dddd [at] LT'),  "Today + " + i + " days current time");
+            test.equal(m.calendar(),       m.format('dddd LT [မှာ]'),  "Today + " + i + " days current time");
             m.hours(0).minutes(0).seconds(0).milliseconds(0);
-            test.equal(m.calendar(),       m.format('dddd [at] LT'),  "Today + " + i + " days beginning of day");
+            test.equal(m.calendar(),       m.format('dddd LT [မှာ]'),  "Today + " + i + " days beginning of day");
             m.hours(23).minutes(59).seconds(59).milliseconds(999);
-            test.equal(m.calendar(),       m.format('dddd [at] LT'),  "Today + " + i + " days end of day");
+            test.equal(m.calendar(),       m.format('dddd LT [မှာ]'),  "Today + " + i + " days end of day");
         }
         test.done();
     },
@@ -223,11 +223,11 @@ exports["lang:my"] = {
 
         for (i = 2; i < 7; i++) {
             m = moment().subtract({ d: i });
-            test.equal(m.calendar(),       m.format('[Last] dddd [at] LT'),  "Today - " + i + " days current time");
+            test.equal(m.calendar(),       m.format('[ပြီးခဲ့သော] dddd LT [မှာ]'),  "Today - " + i + " days current time");
             m.hours(0).minutes(0).seconds(0).milliseconds(0);
-            test.equal(m.calendar(),       m.format('[Last] dddd [at] LT'),  "Today - " + i + " days beginning of day");
+            test.equal(m.calendar(),       m.format('[ပြီးခဲ့သော] dddd LT [မှာ]'),  "Today - " + i + " days beginning of day");
             m.hours(23).minutes(59).seconds(59).milliseconds(999);
-            test.equal(m.calendar(),       m.format('[Last] dddd [at] LT'),  "Today - " + i + " days end of day");
+            test.equal(m.calendar(),       m.format('[ပြီးခဲ့သော] dddd LT [မှာ]'),  "Today - " + i + " days end of day");
         }
         test.done();
     },
@@ -238,14 +238,14 @@ exports["lang:my"] = {
         var weeksAgo = moment().subtract({ w: 1 }),
             weeksFromNow = moment().add({ w: 1 });
 
-        test.equal(weeksAgo.calendar(),       weeksAgo.format('L'),  "လြန္ခဲ့ေသာ ၁ ပတ္က");
-        test.equal(weeksFromNow.calendar(),   weeksFromNow.format('L'),  "၁ ပတ္အတြင္း");
+        test.equal(weeksAgo.calendar(),       weeksAgo.format('L'),  "လွန်ခဲ့သော ၁ ပတ်က");
+        test.equal(weeksFromNow.calendar(),   weeksFromNow.format('L'),  "၁ ပတ်အတွင်း");
 
         weeksAgo = moment().subtract({ w: 2 });
         weeksFromNow = moment().add({ w: 2 });
 
-        test.equal(weeksAgo.calendar(),       weeksAgo.format('L'),  "၂ ပတ္ အရင္က");
-        test.equal(weeksFromNow.calendar(),   weeksFromNow.format('L'),  "၂ ပတ္ အတြင္း");
+        test.equal(weeksAgo.calendar(),       weeksAgo.format('L'),  "၂ ပတ် အရင်က");
+        test.equal(weeksFromNow.calendar(),   weeksFromNow.format('L'),  "၂ ပတ် အတွင်း");
 
         test.done();
     },
@@ -345,15 +345,15 @@ exports["lang:my"] = {
     "weeks year starting sunday formatted" : function (test) {
         test.expect(5);
 
-        test.equal(moment([2012, 0,  1]).format('w ww wo'), '52 52 52nd', "Jan  1 2012 should be week 52");
-        test.equal(moment([2012, 0,  2]).format('w ww wo'),   '1 01 1st', "Jan  2 2012 should be week 1");
-        test.equal(moment([2012, 0,  8]).format('w ww wo'),   '1 01 1st', "Jan  8 2012 should be week 1");
-        test.equal(moment([2012, 0,  9]).format('w ww wo'),   '2 02 2nd', "Jan  9 2012 should be week 2");
-        test.equal(moment([2012, 0, 15]).format('w ww wo'),   '2 02 2nd', "Jan 15 2012 should be week 2");
+        test.equal(moment([2012, 0,  1]).format('w ww wo'), '၅၂ ၅၂ ၅၂', "Jan  1 2012 should be week 52");
+        test.equal(moment([2012, 0,  2]).format('w ww wo'),   '၁ ၀၁ ၁', "Jan  2 2012 should be week 1");
+        test.equal(moment([2012, 0,  8]).format('w ww wo'),   '၁ ၀၁ ၁', "Jan  8 2012 should be week 1");
+        test.equal(moment([2012, 0,  9]).format('w ww wo'),   '၂ ၀၂ ၂', "Jan  9 2012 should be week 2");
+        test.equal(moment([2012, 0, 15]).format('w ww wo'),   '၂ ၀၂ ၂', "Jan 15 2012 should be week 2");
 
         test.done();
     },
-*/
+
     "returns the name of the language" : function (test) {
         if (typeof module !== 'undefined' && module.exports) {
             test.equal(require('../../lang/my'), 'my', "module should export my");
