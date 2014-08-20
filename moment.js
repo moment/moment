8e12f053d16cffd939d405766407fda032896cc5
Iskren Chernev <iskren.chernev@gmail.com>
2014-08-20 23:02:43 +0300

    Add moment.duration.toString()

--- moment.js
+++ moment.js
@@ -2712,6 +2712,8 @@
         }
     });
 
+    moment.duration.fn.toString = moment.duration.fn.toISOString;
+
     function makeDurationGetter(name) {
         moment.duration.fn[name] = function () {
             return this._data[name];
