713fef3faf07ac93944acad970cb43626f1e735f
Jared Menard <edgartheunready@users.noreply.github.com>
2014-10-02 14:24:10 -0400

    removing double quotes

--- locale/rw.js
+++ locale/rw.js
@@ -11,19 +11,19 @@
     }
 }(function (moment) {
     return moment.defineLocale('rw', {
-        months : "Mutarama_Gashyantare_Werurwe_Mata_Gicuransi_Kamena_Nyakanga_Kanama_Nzeli_Ukwakira_Ugushyingo_Ukuboza".split("_"),
-        monthsShort : "Mut_Gas_Wer_Mat_Gic_Kam_Nya_Kan_Nze_Ukw_Ugu_Uku".split("_"),
-        weekdays : "Ku cyumweru_Kuwa mbere_Kuwa kabiri_Kuwa gatatu_Kuwa kane_Kuwa gatanu_Kuwa gatandatu".split("_"),
-        weekdaysShort : "Cyu_Mbe_Kab_Gtu_Kan_Gnu_Gnd".split("_"),
-        weekdaysMin : "Cy_Mb_Ka_Gt_Ka_Gn_Gd".split("_"),
+        months : 'Mutarama_Gashyantare_Werurwe_Mata_Gicuransi_Kamena_Nyakanga_Kanama_Nzeli_Ukwakira_Ugushyingo_Ukuboza'.split('_'),
+        monthsShort : 'Mut_Gas_Wer_Mat_Gic_Kam_Nya_Kan_Nze_Ukw_Ugu_Uku'.split('_'),
+        weekdays : 'Ku cyumweru_Kuwa mbere_Kuwa kabiri_Kuwa gatatu_Kuwa kane_Kuwa gatanu_Kuwa gatandatu'.split('_'),
+        weekdaysShort : 'Cyu_Mbe_Kab_Gtu_Kan_Gnu_Gnd'.split('_'),
+        weekdaysMin : 'Cy_Mb_Ka_Gt_Ka_Gn_Gd'.split('_'),
         longDateFormat : {
-            LT : "h:mm A",
-            L : "DD/MM/YYYY",
-            LL : "D MMMM YYYY",
-            LLL : "D MMMM YYYY LT",
-            LLLL : "dddd, D MMMM YYYY LT"
+            LT : 'h:mm A',
+            L : 'DD/MM/YYYY',
+            LL : 'D MMMM YYYY',
+            LLL : 'D MMMM YYYY LT',
+            LLLL : 'dddd, D MMMM YYYY LT'
         },
-        calendar : {
+        calendar : 
             sameDay : '[Today at] LT',
             nextDay : '[Tomorrow at] LT',
             nextWeek : 'dddd [at] LT',
@@ -32,19 +32,19 @@
             sameElse : 'L'
         },
         relativeTime : {
-            future : "in %s",
-            past : "%s ago",
-            s : "a few seconds",
-            m : "a minute",
-            mm : "%d minutes",
-            h : "an hour",
-            hh : "%d hours",
-            d : "a day",
-            dd : "%d days",
-            M : "a month",
-            MM : "%d months",
-            y : "a year",
-            yy : "%d years"
+            future : 'in %s',
+            past : '%s ago',
+            s : 'a few seconds',
+            m : 'a minute',
+            mm : '%d minutes',
+            h : 'an hour',
+            hh : '%d hours',
+            d : 'a day',
+            dd : '%d days',
+            M : 'a month',
+            MM : '%d months',
+            y : 'a year',
+            yy : '%d years'
         },
         ordinal : function (number) {
             var b = number % 10,
