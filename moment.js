95be1e31c0069d36c2045fa2302904ada1df95ba
Remy Blom <remy.blom@hku.nl>
2014-10-09 09:23:09 +0200

    removed the unixms function in favor of valueOf()

--- moment.js
+++ moment.js
@@ -272,7 +272,7 @@
                 return this.zoneName();
             },
             x    : function () {
-                return this.unixms();
+                return this.valueOf();
             },
             X    : function () {
                 return this.unix();
@@ -1859,11 +1859,6 @@
         return makeMoment(c).utc();
     };
 
-    // creating with unix offset (in milliseconds)
-    moment.unixms = function (input) {
-        return moment(input);
-    };
-
     // creating with unix timestamp (in seconds)
     moment.unix = function (input) {
         return moment(input * 1000);
@@ -2098,10 +2093,6 @@
 
         valueOf : function () {
             return +this._d + ((this._offset || 0) * 60000);
-        },
-
-        unixms: function () {
-            return +this;
         },
 
         unix : function () {
