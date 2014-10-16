2a9aa43fad59e7976417b42be3d2483339d7af9d
Anton Wilhelm <timaschew@gmail.com>
2014-10-16 15:46:54 +0200

    require 'moment' in locales instead of '../momemt'

--- locale/he.js
+++ locale/he.js
@@ -8,7 +8,7 @@
     if (typeof define === 'function' && define.amd) {
         define(['moment'], factory); // AMD
     } else if (typeof exports === 'object') {
-        module.exports = factory(require('../moment')); // Node
+        module.exports = factory(require('moment')); // Node
     } else {
         factory(window.moment); // Browser global
     }
