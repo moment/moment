a1e8a02e508b427a9e8eb0178ffffd5f3cbad54c
elad <elad@iNNU.ORG>
2014-10-29 17:12:12 +0200

    Call updateOffset on moment creation

--- moment.js
+++ moment.js
@@ -404,6 +404,9 @@
         }
         copyConfig(this, config);
         this._d = new Date(+config._d);
+        if (moment.updateOffset) {
+            moment.updateOffset(this);
+        }
     }
 
     // Duration Constructor
