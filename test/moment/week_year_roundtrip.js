bcccc6d38b0263332e2ff49d2fdcb093c76bacd3
Gregor Rayman <gregor.rayman@cloudfarms.com>
2015-01-29 16:40:55 +0100

    Added a specific test for the dow/doy combination 6/12 for the weeks around 2012/2013

--- test/moment/week_year_roundtrip.js
+++ test/moment/week_year_roundtrip.js
@@ -28,5 +28,18 @@ exports.weekYearRoundtrip = {
         }
 
         test.done();
+    },
+
+    'week numbers 2012/2013': function (test) {
+        test.expect(7);
+        moment.locale(moment.locale(), {week: {dow: 6, doy: 12}});
+        test.equal(52, moment('2012-12-28', 'YYYY-MM-DD').week()); // 51 -- should be 52?
+        test.equal(1, moment('2012-12-29', 'YYYY-MM-DD').week()); // 52 -- should be 1
+        test.equal(1, moment('2013-01-01', 'YYYY-MM-DD').week()); // 52 -- should be 1
+        test.equal(2, moment('2013-01-08', 'YYYY-MM-DD').week()); // 53 -- should be 2
+        test.equal(2, moment('2013-01-11', 'YYYY-MM-DD').week()); // 53 -- should be 2
+        test.equal(3, moment('2013-01-12', 'YYYY-MM-DD').week()); // 1 -- should be 3
+        test.equal(52, moment().weeksInYear(2012)); // 52
+        test.done();
     }
 };
