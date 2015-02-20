3c76dcbb00969bf77fce6401fa6d82b110e9c64f
Fahad Kassim <fahad@yoteyote.com>
2015-02-20 15:08:21 +0200

    Deleted WhiteSpace

--- locale/sw.js
+++ locale/sw.js
@@ -47,7 +47,7 @@
         	MM : 'miezi %d',
         	y : 'mwaka mmoja',
         	yy : 'miaka %d'
-        },        
+        },
         week : {
             dow : 1, // Monday is the first day of the week.
             doy : 7  // The week that contains Jan 1st is the first week of the year.
