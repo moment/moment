9bdb05e25920d64834f9c56375ebe3c0aaf50cc5
Andrew Becker <abecks@gmail.com>
2015-04-06 12:07:27 -0700

    Fix for IE8

--- meteor/export.js
+++ meteor/export.js
@@ -1,3 +1,5 @@
 // moment.js makes `moment` global on the window (or global) object, while Meteor expects a file-scoped global variable
 moment = this.moment;
-delete this.moment;
+try{
+  delete this.moment;
+}catch(e){}
