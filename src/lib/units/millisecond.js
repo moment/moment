964f17eb3af45b6b8f34ef8f9fd14776ec8ac7bf
Iskren Chernev <iskren.chernev@gmail.com>
2015-07-06 00:28:40 +0900

    Support up to milliseconds tokens up to 9 digits, fixes #2436

--- src/lib/units/millisecond.js
+++ src/lib/units/millisecond.js
@@ -16,12 +16,26 @@ addFormatToken(0, ['SS', 2], 0, function () {
     return ~~(this.millisecond() / 10);
 });
 
-function milliseconds (token) {
-    addFormatToken(0, [token, 3], 0, 'millisecond');
-}
+addFormatToken(0, ['SSS', 3], 0, 'millisecond');
+addFormatToken(0, ['SSSS', 4], 0, function () {
+    return this.millisecond() * 10;
+});
+addFormatToken(0, ['SSSSS', 5], 0, function () {
+    return this.millisecond() * 100;
+});
+addFormatToken(0, ['SSSSSS', 6], 0, function () {
+    return this.millisecond() * 1000;
+});
+addFormatToken(0, ['SSSSSSS', 7], 0, function () {
+    return this.millisecond() * 10000;
+});
+addFormatToken(0, ['SSSSSSSS', 8], 0, function () {
+    return this.millisecond() * 100000;
+});
+addFormatToken(0, ['SSSSSSSSS', 9], 0, function () {
+    return this.millisecond() * 1000000;
+});
 
-milliseconds('SSS');
-milliseconds('SSSS');
 
 // ALIASES
 
@@ -33,7 +47,14 @@ addRegexToken('S',    match1to3, match1);
 addRegexToken('SS',   match1to3, match2);
 addRegexToken('SSS',  match1to3, match3);
 addRegexToken('SSSS', matchUnsigned);
-addParseToken(['S', 'SS', 'SSS', 'SSSS'], function (input, array) {
+addRegexToken('SSSSS', matchUnsigned);
+addRegexToken('SSSSSS', matchUnsigned);
+addRegexToken('SSSSSSS', matchUnsigned);
+addRegexToken('SSSSSSSS', matchUnsigned);
+addRegexToken('SSSSSSSSS', matchUnsigned);
+addParseToken(
+        'S SS SSS SSSS SSSSS SSSSSS SSSSSSS SSSSSSSS SSSSSSSSS'.split(' '),
+        function (input, array) {
     array[MILLISECOND] = toInt(('0.' + input) * 1000);
 });
 
