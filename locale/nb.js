0c7b0623d086d68cabf1f424ec4d32f8ed63a1b2
Sigurd Gartmann <sigurdga@sigurdga.no>
2015-07-08 23:15:13 +0200

    Colon has been approved as separator between hours, minutes and seconds
    This has been part of the approved ISO 8601 standard for several years, and was
    officially taken into the language in 2014 (source:
    http://www.korrekturavdelingen.no/K4Punktum.htm).
    
    According to the ISO standard, hours should be noted between 00 and 24, so now
    using HH for hours.

--- locale/nb.js
+++ locale/nb.js
@@ -17,8 +17,8 @@
         weekdaysShort : 'søn_man_tirs_ons_tors_fre_lør'.split('_'),
         weekdaysMin : 'sø_ma_ti_on_to_fr_lø'.split('_'),
         longDateFormat : {
-            LT : 'H.mm',
-            LTS : 'LT.ss',
+            LT : 'HH:mm',
+            LTS : 'LT:ss',
             L : 'DD.MM.YYYY',
             LL : 'D. MMMM YYYY',
             LLL : 'D. MMMM YYYY [kl.] LT',
