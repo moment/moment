1b37624456f4d01017535b616ccf78f3373d4b84
Iskren Chernev <iskren.chernev@gmail.com>
2015-07-12 16:50:56 -0700

    Revert "Merge pull request #2347 from wajatimur:develop"
    I'll redo this change myself.
    
    This reverts commit 779eab554d6af2186496a8743729da3cf85196a3, reversing
    changes made to 0137bc1049b01fcf8f7a2f91db2f5c1f65577e54.

--- locale/ms-my.js
+++ locale/ms-my.js
@@ -9,7 +9,7 @@
 }(this, function (moment) { 'use strict';
 
 
-    var ms_my = moment.defineLocale('ms', {
+    var ms_my = moment.defineLocale('ms-my', {
         months : 'Januari_Februari_Mac_April_Mei_Jun_Julai_Ogos_September_Oktober_November_Disember'.split('_'),
         monthsShort : 'Jan_Feb_Mac_Apr_Mei_Jun_Jul_Ogs_Sep_Okt_Nov_Dis'.split('_'),
         weekdays : 'Ahad_Isnin_Selasa_Rabu_Khamis_Jumaat_Sabtu'.split('_'),
