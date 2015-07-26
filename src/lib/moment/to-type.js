26f5fb424ffd6cda18b71f927804df076efe61c3
Iskren Chernev <iskren.chernev@gmail.com>
2015-07-25 20:00:04 -0700

    Redo #2123 in es6

--- src/lib/moment/to-type.js
+++ src/lib/moment/to-type.js
@@ -14,3 +14,16 @@ export function toArray () {
     var m = this;
     return [m.year(), m.month(), m.date(), m.hour(), m.minute(), m.second(), m.millisecond()];
 }
+
+export function toObject () {
+    var m = this;
+    return {
+        years: m.year(),
+        months: m.month(),
+        date: m.date(),
+        hours: m.hours(),
+        minutes: m.minutes(),
+        seconds: m.seconds(),
+        milliseconds: m.milliseconds()
+    };
+}
