3fc8774383f56d71028cb64d03b66a19b21d39ef
Raphael Amorim <rapha850@gmail.com>
2015-07-30 01:04:46 -0300

    ajust tests for fixed nomenclature (fr-ca)

--- src/test/locale/fr-ca.js
+++ src/test/locale/fr-ca.js
@@ -4,7 +4,7 @@ localeModule('fr-ca');
 
 test('parse', function (assert) {
     var i,
-        tests = 'janvier janv._février févr._mars mars_avril avr._mai mai_juin juin_juillet juil._août août_septembre sept._octobre oct._novembre nov._décembre déc.'.split('_');
+        tests = 'Janvier Janv._Février Févr._Mars Mars_Avril Avr._Mai Mai_Juin Juin_Juillet Juil._Août Août_Septembre Sept._Octobre Oct._Novembre Nov._Décembre Déc.'.split('_');
 
     function equalTest(input, mmm, i) {
         assert.equal(moment(input, mmm).month(), i, input + ' should be month ' + (i + 1));
@@ -25,12 +25,12 @@ test('parse', function (assert) {
 
 test('format', function (assert) {
     var a = [
-            ['dddd, MMMM Do YYYY, h:mm:ss a',      'dimanche, février 14e 2010, 3:25:50 pm'],
-            ['ddd, hA',                            'dim., 3PM'],
-            ['M Mo MM MMMM MMM',                   '2 2e 02 février févr.'],
+            ['dddd, MMMM Do YYYY, h:mm:ss a',      'Dimanche, Février 14e 2010, 3:25:50 pm'],
+            ['ddd, hA',                            'Dim., 3PM'],
+            ['M Mo MM MMMM MMM',                   '2 2e 02 Février Févr.'],
             ['YYYY YY',                            '2010 10'],
             ['D Do DD',                            '14 14e 14'],
-            ['d do dddd ddd dd',                   '0 0e dimanche dim. Di'],
+            ['d do dddd ddd dd',                   '0 0e Dimanche Dim. Di'],
             ['DDD DDDo DDDD',                      '45 45e 045'],
             ['w wo ww',                            '8 8e 08'],
             ['h hh',                               '3 03'],
@@ -41,13 +41,13 @@ test('format', function (assert) {
             ['[the] DDDo [day of the year]',       'the 45e day of the year'],
             ['LTS',                                '15:25:50'],
             ['L',                                  '2010-02-14'],
-            ['LL',                                 '14 février 2010'],
-            ['LLL',                                '14 février 2010 15:25'],
-            ['LLLL',                               'dimanche 14 février 2010 15:25'],
+            ['LL',                                 '14 Février 2010'],
+            ['LLL',                                '14 Février 2010 15:25'],
+            ['LLLL',                               'Dimanche 14 Février 2010 15:25'],
             ['l',                                  '2010-2-14'],
-            ['ll',                                 '14 févr. 2010'],
-            ['lll',                                '14 févr. 2010 15:25'],
-            ['llll',                               'dim. 14 févr. 2010 15:25']
+            ['ll',                                 '14 Févr. 2010'],
+            ['lll',                                '14 Févr. 2010 15:25'],
+            ['llll',                               'Dim. 14 Févr. 2010 15:25']
         ],
         b = moment(new Date(2010, 1, 14, 15, 25, 50, 125)),
         i;
@@ -96,7 +96,7 @@ test('format ordinal', function (assert) {
 
 test('format month', function (assert) {
     var i,
-        expected = 'janvier janv._février févr._mars mars_avril avr._mai mai_juin juin_juillet juil._août août_septembre sept._octobre oct._novembre nov._décembre déc.'.split('_');
+        expected = 'Janvier Janv._Février Févr._Mars Mars_Avril Avr._Mai Mai_Juin Juin_Juillet Juil._Août Août_Septembre Sept._Octobre Oct._Novembre Nov._Décembre Déc.'.split('_');
 
     for (i = 0; i < expected.length; i++) {
         assert.equal(moment([2011, i, 1]).format('MMMM MMM'), expected[i], expected[i]);
@@ -105,7 +105,7 @@ test('format month', function (assert) {
 
 test('format week', function (assert) {
     var i,
-        expected = 'dimanche dim. Di_lundi lun. Lu_mardi mar. Ma_mercredi mer. Me_jeudi jeu. Je_vendredi ven. Ve_samedi sam. Sa'.split('_');
+        expected = 'Dimanche Dim. Di_Lundi Lun. Lu_Mardi Mar. Ma_Mercredi Mer. Me_Jeudi Jeu. Je_Vendredi Ven. Ve_Samedi Sam. Sa'.split('_');
 
     for (i = 0; i < expected.length; i++) {
         assert.equal(moment([2011, 0, 2 + i]).format('dddd ddd dd'), expected[i], expected[i]);
