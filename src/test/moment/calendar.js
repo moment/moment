4267242750e7a778b3b59b6ecab4c20651b8f985
Timothy Stephens <timothy.f.stephens@gmail.com>
2015-09-21 12:42:10 -0400

    Added extra test, wrapped type check in parentheses

--- src/test/moment/calendar.js
+++ src/test/moment/calendar.js
@@ -4,9 +4,12 @@ import moment from '../../moment';
 test('calendar function with functions', function (assert) {
     var m = moment(),
         format = {
-            sameDay: function() { return '[Today]'; },
+            sameDay: function() { return '[Today2]'; },
             nextDay: function() { return '[Tomorrow]'; }
-        };
-    assert.equal(m.calendar(null, format), 'Today', 'calendar using format function - Today')
-    assert.equal(m.add(1, 'day').calendar(null, format), 'Tomorrow', 'calendar using format function - Tomorrow')
+        },
+        preformat = m.calendar();
+
+    assert.equal(m.calendar(null, format), 'Today2', 'calendar using format function - Today');
+    assert.equal(m.calendar(), preformat, 'calendar using no format');
+    assert.equal(m.add(1, 'day').calendar(null, format), 'Tomorrow', 'calendar using format function - Tomorrow');
 });
