9bb059a8bf9704312b6dbc38f9d611c3939693fa
Vivek Athalye <vnathalye@users.noreply.github.com>
2015-09-22 12:22:19 +0530

    formatting related changes
    Changed double quotes to single quotes and added space after switch
    keyword.

--- src/locale/mr.js
+++ src/locale/mr.js
@@ -32,35 +32,35 @@ numberMap = {
 
 function relativeTimeMr(number, withoutSuffix, string, isFuture)
 {
-    var output = "";
+    var output = '';
     if (withoutSuffix) {
-        switch(string) {
-            case "s": output = 'काही सेकंद'; break;
-            case "m": output = 'एक मिनिट'; break;
-            case "mm": output = '%d मिनिटे'; break;
-            case "h": output = 'एक तास'; break;
-            case "hh": output = '%d तास'; break;
-            case "d": output = 'एक दिवस'; break;
-            case "dd": output = '%d दिवस'; break;
-            case "M": output = 'एक महिना'; break;
-            case "MM": output = '%d महिने'; break;
-            case "y": output = 'एक वर्ष'; break;
-            case "yy": output = '%d वर्षे'; break;
+        switch (string) {
+            case 's': output = 'काही सेकंद'; break;
+            case 'm': output = 'एक मिनिट'; break;
+            case 'mm': output = '%d मिनिटे'; break;
+            case 'h': output = 'एक तास'; break;
+            case 'hh': output = '%d तास'; break;
+            case 'd': output = 'एक दिवस'; break;
+            case 'dd': output = '%d दिवस'; break;
+            case 'M': output = 'एक महिना'; break;
+            case 'MM': output = '%d महिने'; break;
+            case 'y': output = 'एक वर्ष'; break;
+            case 'yy': output = '%d वर्षे'; break;
         }
     }
     else {
         switch (string) {
-            case "s": output = 'काही सेकंदां'; break;
-            case "m": output = 'एका मिनिटा'; break;
-            case "mm": output = '%d मिनिटां'; break;
-            case "h": output = 'एका तासा'; break;
-            case "hh": output = '%d तासां'; break;
-            case "d": output = 'एका दिवसा'; break;
-            case "dd": output = '%d दिवसां'; break;
-            case "M": output = 'एका महिन्या'; break;
-            case "MM": output = '%d महिन्यां'; break;
-            case "y": output = 'एका वर्षा'; break;
-            case "yy": output = '%d वर्षां'; break;
+            case 's': output = 'काही सेकंदां'; break;
+            case 'm': output = 'एका मिनिटा'; break;
+            case 'mm': output = '%d मिनिटां'; break;
+            case 'h': output = 'एका तासा'; break;
+            case 'hh': output = '%d तासां'; break;
+            case 'd': output = 'एका दिवसा'; break;
+            case 'dd': output = '%d दिवसां'; break;
+            case 'M': output = 'एका महिन्या'; break;
+            case 'MM': output = '%d महिन्यां'; break;
+            case 'y': output = 'एका वर्षा'; break;
+            case 'yy': output = '%d वर्षां'; break;
         }
     }
     return output.replace(/%d/i, number);
