fa77d561d5e0ec4cb393c6426f2b95c5a76d80a9
Massimiliano Caniparoli <massic80@gmail.com>
2015-09-24 11:39:20 +0200

    Same length for weekdaysMin parts (Italian) ES6
    Rebasing, as discussed here https://github.com/moment/moment/pull/2237

--- src/locale/it.js
+++ src/locale/it.js
@@ -10,7 +10,7 @@ export default moment.defineLocale('it', {
     monthsShort : 'gen_feb_mar_apr_mag_giu_lug_ago_set_ott_nov_dic'.split('_'),
     weekdays : 'Domenica_Lunedì_Martedì_Mercoledì_Giovedì_Venerdì_Sabato'.split('_'),
     weekdaysShort : 'Dom_Lun_Mar_Mer_Gio_Ven_Sab'.split('_'),
-    weekdaysMin : 'D_L_Ma_Me_G_V_S'.split('_'),
+    weekdaysMin : 'Do_Lu_Ma_Me_Gi_Ve_Sa'.split('_'),
     longDateFormat : {
         LT : 'HH:mm',
         LTS : 'HH:mm:ss',
