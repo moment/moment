6173b268a8c0aca8075b781d52b9a87f98ed28e5
Jawish Hameed <jawish@gmail.com>
2015-10-01 05:06:30 +0500

    Convert to Unicode
    Changed Dhivehi escape sequences to real Unicode characters

--- src/locale/dv.js
+++ src/locale/dv.js
@@ -5,34 +5,34 @@
 import moment from '../moment';
 
 var months = [
-    '\u0796\u07ac\u0782\u07aa\u0787\u07a6\u0783\u07a9',
-    '\u078a\u07ac\u0784\u07aa\u0783\u07aa\u0787\u07a6\u0783\u07a9',
-    '\u0789\u07a7\u0783\u07a8\u0797\u07aa',
-    '\u0787\u07ad\u0795\u07b0\u0783\u07a9\u078d\u07aa',
-    '\u0789\u07ad',
-    '\u0796\u07ab\u0782\u07b0',
-    '\u0796\u07aa\u078d\u07a6\u0787\u07a8',
-    '\u0787\u07af\u078e\u07a6\u0790\u07b0\u0793\u07aa',
-    '\u0790\u07ac\u0795\u07aa\u0793\u07ac\u0789\u07b0\u0784\u07a6\u0783\u07aa',
-    '\u0787\u07ae\u0786\u07b0\u0793\u07af\u0784\u07a6\u0783\u07aa',
-    '\u0782\u07ae\u0788\u07ac\u0789\u07b0\u0784\u07a6\u0783\u07aa',
-    '\u0791\u07a8\u0790\u07ac\u0789\u07b0\u0784\u07a6\u0783\u07aa'
+    'ޖެނުއަރީ',
+    'ފެބްރުއަރީ',
+    'މާރިޗު',
+    'އޭޕްރީލު',
+    'މޭ',
+    'ޖޫން',
+    'ޖުލައި',
+    'އޯގަސްޓު',
+    'ސެޕްޓެމްބަރު',
+    'އޮކްޓޯބަރު',
+    'ނޮވެމްބަރު',
+    'ޑިސެމްބަރު'
+], weekdays = [
+    'އާދިއްތަ',
+    'ހޯމަ',
+    'އަންގާރަ',
+    'ބުދަ',
+    'ބުރާސްފަތި',
+    'ހުކުރު',
+    'ހޮނިހިރު'
 ];
 
 export default moment.defineLocale('dv', {
     months : months,
     monthsShort : months,
-    weekdays : [
-        '\u0787\u07a7\u078b\u07a8\u0787\u07b0\u078c\u07a6',
-        '\u0780\u07af\u0789\u07a6',
-        '\u0787\u07a6\u0782\u07b0\u078e\u07a7\u0783\u07a6',
-        '\u0784\u07aa\u078b\u07a6',
-        '\u0784\u07aa\u0783\u07a7\u0790\u07b0\u078a\u07a6\u078c\u07a8',
-        '\u0780\u07aa\u0786\u07aa\u0783\u07aa',
-        '\u0780\u07ae\u0782\u07a8\u0780\u07a8\u0783\u07aa'
-    ],
-    weekdaysShort : '\u0787\u07a7\u078b\u07a8_\u0780\u07af\u0789\u07a6_\u0787\u07a6\u0782\u07b0_\u0784\u07aa\u078b\u07a6_\u0784\u07aa\u0783\u07a7_\u0780\u07aa\u0786\u07aa_\u0780\u07ae\u0782\u07a8'.split('_'),
-    weekdaysMin : '\u0787\u078b_\u0780\u0789_\u0787\u0782_\u0784\u078b_\u0784\u0783_\u0780\u0786_\u0780\u0782'.split('_'),
+    weekdays : weekdays,
+    weekdaysShort : weekdays,
+    weekdaysMin : 'އާދި_ހޯމަ_އަން_ބުދަ_ބުރާ_ހުކު_ހޮނި'.split('_'),
     longDateFormat : {
 
         LT : 'HH:mm',
@@ -42,39 +42,39 @@ export default moment.defineLocale('dv', {
         LLL : 'D MMMM YYYY HH:mm',
         LLLL : 'dddd D MMMM YYYY HH:mm'
     },
-    meridiemParse: /\u0789\u0786|\u0789\u078a/,
+    meridiemParse: /މކ|މފ/,
     isPM : function (input) {
-        return '\u0789\u078a' === input;
+        return '' === input;
     },
     meridiem : function (hour, minute, isLower) {
         if (hour < 12) {
-            return '\u0789\u0786';
+            return 'މކ';
         } else {
-            return '\u0789\u078a';
+            return 'މފ';
         }
     },
     calendar : {
-        sameDay : '[\u0789\u07a8\u0787\u07a6\u078b\u07aa] LT',
-        nextDay : '[\u0789\u07a7\u078b\u07a6\u0789\u07a7] LT',
+        sameDay : '[މިއަދު] LT',
+        nextDay : '[މާދަމާ] LT',
         nextWeek : 'dddd LT',
-        lastDay : '[\u0787\u07a8\u0787\u07b0\u0794\u07ac] LT',
-        lastWeek : '[\u078a\u07a7\u0787\u07a8\u078c\u07aa\u0788\u07a8] dddd LT',
+        lastDay : '[އިއްޔެ] LT',
+        lastWeek : '[ފާއިތުވި] dddd LT',
         sameElse : 'L'
     },
     relativeTime : {
-        future : '\u078c\u07ac\u0783\u07ad\u078e\u07a6\u0787\u07a8 %s',
-        past : '\u0786\u07aa\u0783\u07a8\u0782\u07b0 %s',
-        s : '\u0790\u07a8\u0786\u07aa\u0782\u07b0\u078c\u07aa\u0786\u07ae\u0785\u07ac\u0787\u07b0',
-        m : '\u0789\u07a8\u0782\u07a8\u0793\u07ac\u0787\u07b0',
-        mm : '\u0789\u07a8\u0782\u07a8\u0793\u07aa %d',
-        h : '\u078e\u07a6\u0791\u07a8\u0787\u07a8\u0783\u07ac\u0787\u07b0',
-        hh : '\u078e\u07a6\u0791\u07a8\u0787\u07a8\u0783\u07aa %d',
-        d : '\u078b\u07aa\u0788\u07a6\u0780\u07ac\u0787\u07b0',
-        dd : '\u078b\u07aa\u0788\u07a6\u0790\u07b0 %d',
-        M : '\u0789\u07a6\u0780\u07ac\u0787\u07b0',
-        MM : '\u0789\u07a6\u0790\u07b0 %d',
-        y : '\u0787\u07a6\u0780\u07a6\u0783\u07ac\u0787\u07b0',
-        yy : '\u0787\u07a6\u0780\u07a6\u0783\u07aa %d'
+        future : 'ތެރޭގައި %s',
+        past : 'ކުރިން %s',
+        s : 'ސިކުންތުކޮޅެއް',
+        m : 'މިނިޓެއް',
+        mm : 'މިނިޓު %d',
+        h : 'ގަޑިއިރެއް',
+        hh : 'ގަޑިއިރު %d',
+        d : 'ދުވަހެއް',
+        dd : 'ދުވަސް %d',
+        M : 'މަހެއް',
+        MM : 'މަސް %d',
+        y : 'އަހަރެއް',
+        yy : 'އަހަރު %d'
     },
     preparse: function (string) {
         return string.replace(/،/g, ',');
