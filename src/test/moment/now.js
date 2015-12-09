331029bbc1c29f455c9604d7b20a0255f2940994
Iskren Chernev <iskren.chernev@gmail.com>
2015-12-09 00:13:26 -0800

    Fix tests for all timezones

--- src/test/moment/now.js
+++ src/test/moment/now.js
@@ -23,7 +23,7 @@ test('now - custom value', function (assert) {
 
     try {
         assert.ok(moment().toISOString() === CUSTOM_DATE, 'moment() constructor should use the function defined by moment.now, but it did not');
-        assert.ok(moment([]).toISOString() === '2015-01-01T00:00:00.000Z', 'moment() constructor should fall back to the date defined by moment.now when an empty array is given, but it did not');
+        assert.ok(moment.utc().toISOString() === CUSTOM_DATE, 'moment() constructor should use the function defined by moment.now, but it did not');
         assert.ok(moment.utc([]).toISOString() === '2015-01-01T00:00:00.000Z', 'moment() constructor should fall back to the date defined by moment.now when an empty array is given, but it did not');
     } finally {
         moment.fn.now = oldFn;
