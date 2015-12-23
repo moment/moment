0897f50b38e94fb397cc10239c46767c4095ce3e
Iskren Chernev <iskren.chernev@gmail.com>
2015-12-22 20:38:42 -0800

    Fix forgotten merge note in #2646

--- src/lib/create/date-from-array.js
+++ src/lib/create/date-from-array.js
@@ -4,7 +4,7 @@ export function createDate (y, m, d, h, M, s, ms) {
     var date = new Date(y, m, d, h, M, s, ms);
 
     //the date constructor remaps years 0-99 to 1900-1999
-    if (y < 100 && y >= 0 && isFinite(date.getYear())) {
+    if (y < 100 && y >= 0 && isFinite(date.getFullYear())) {
         date.setFullYear(y);
     }
     return date;
