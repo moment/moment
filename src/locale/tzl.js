fb3fb8b82ccdb62afda5b6b7855b0d158c1cf159
Iskren Chernev <iskren.chernev@gmail.com>
2015-12-31 11:58:43 +0200

    Do not use String.trim for IE8 compat

--- src/locale/tzl.js
+++ src/locale/tzl.js
@@ -63,16 +63,16 @@ function processRelativeTime(number, withoutSuffix, key, isFuture) {
     var format = {
         's': ['viensas secunds', '\'iensas secunds'],
         'm': ['\'n míut', '\'iens míut'],
-        'mm': [number + ' míuts', ' ' + number + ' míuts'],
+        'mm': [number + ' míuts', '' + number + ' míuts'],
         'h': ['\'n þora', '\'iensa þora'],
-        'hh': [number + ' þoras', ' ' + number + ' þoras'],
+        'hh': [number + ' þoras', '' + number + ' þoras'],
         'd': ['\'n ziua', '\'iensa ziua'],
-        'dd': [number + ' ziuas', ' ' + number + ' ziuas'],
+        'dd': [number + ' ziuas', '' + number + ' ziuas'],
         'M': ['\'n mes', '\'iens mes'],
-        'MM': [number + ' mesen', ' ' + number + ' mesen'],
+        'MM': [number + ' mesen', '' + number + ' mesen'],
         'y': ['\'n ar', '\'iens ar'],
-        'yy': [number + ' ars', ' ' + number + ' ars']
+        'yy': [number + ' ars', '' + number + ' ars']
     };
-    return isFuture ? format[key][0] : (withoutSuffix ? format[key][0] : format[key][1].trim());
+    return isFuture ? format[key][0] : (withoutSuffix ? format[key][0] : format[key][1]);
 }
 
