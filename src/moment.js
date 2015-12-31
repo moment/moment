6effd83b71ecc8f2f84c94a6705c75e17eb2e871
Iskren Chernev <iskren.chernev@gmail.com>
2015-12-31 14:48:17 +0200

    Bump version to 2.11.0

--- src/moment.js
+++ src/moment.js
@@ -1,12 +1,12 @@
 //! moment.js
-//! version : 2.10.6
+//! version : 2.11.0
 //! authors : Tim Wood, Iskren Chernev, Moment.js contributors
 //! license : MIT
 //! momentjs.com
 
 import { hooks as moment, setHookCallback } from './lib/utils/hooks';
 
-moment.version = '2.10.6';
+moment.version = '2.11.0';
 
 import {
     min,
