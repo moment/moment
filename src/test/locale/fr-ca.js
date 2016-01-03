8d57cce54a2eab5e39555311eb273f611696b810
Iskren Chernev <iskren.chernev@gmail.com>
2016-01-03 14:58:53 +0200

    Remove 'strict oridnal parsign' from all locales

--- src/test/locale/fr-ca.js
+++ src/test/locale/fr-ca.js
@@ -277,11 +277,3 @@ test('weeks year starting sunday format', function (assert) {
 
 
 
-test('strict ordinal parsing', function (assert) {
-    var i, ordinalStr, testMoment;
-    for (i = 1; i <= 31; ++i) {
-        ordinalStr = moment([2014, 0, i]).format('YYYY MM Do');
-        testMoment = moment(ordinalStr, 'YYYY MM Do', true);
-        assert.ok(testMoment.isValid(), 'strict ordinal parsing ' + i);
-    }
-});
