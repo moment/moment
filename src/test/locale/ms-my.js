211c5ad65f8fa4120286e0e40fad0d8320d7b463
Iskren Chernev <iskren.chernev@gmail.com>
2016-01-03 14:56:42 +0200

    Remove 'lenient ordinal parsing of numbers'

--- src/test/locale/ms-my.js
+++ src/test/locale/ms-my.js
@@ -290,18 +290,6 @@ test('meridiem invariant', function (assert) {
     }
 });
 
-test('lenient ordinal parsing of number', function (assert) {
-    var i, testMoment;
-    for (i = 1; i <= 31; ++i) {
-        testMoment = moment('2014 01 ' + i, 'YYYY MM Do');
-        assert.equal(testMoment.year(), 2014,
-                'lenient ordinal parsing of number ' + i + ' year check');
-        assert.equal(testMoment.month(), 0,
-                'lenient ordinal parsing of number ' + i + ' month check');
-        assert.equal(testMoment.date(), i,
-                'lenient ordinal parsing of number ' + i + ' date check');
-    }
-});
 
 test('strict ordinal parsing', function (assert) {
     var i, ordinalStr, testMoment;
