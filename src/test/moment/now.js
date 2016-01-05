moment/moment

commit b1d686ed481e2211cb81a1c60eca850fcea93f21

Use moment.now instead of moment.fn.now for changing time source

src/test/moment/now.js
@@ -15,9 +15,9 @@ test('now', function (assert) {
 test('now - custom value', function (assert) {
     var CUSTOM_DATE = '2015-01-01T01:30:00.000Z';
 
-    var oldFn = moment.fn.now;
+    var oldFn = moment.now;
 
-    moment.fn.now = function () {
+    moment.now = function () {
         return new Date(CUSTOM_DATE).valueOf();
     };
 
@@ -26,6 +26,6 @@ test('now - custom value', function (assert) {
         assert.ok(moment.utc().toISOString() === CUSTOM_DATE, 'moment() constructor should use the function defined by moment.now, but it did not');
         assert.ok(moment.utc([]).toISOString() === '2015-01-01T00:00:00.000Z', 'moment() constructor should fall back to the date defined by moment.now when an empty array is given, but it did not');
     } finally {
-        moment.fn.now = oldFn;
+        moment.now = oldFn;
     }
 });

src/lib/create/from-anything.js
@@ -61,7 +61,7 @@ export function prepareConfig (config) {
 function configFromInput(config) {
     var input = config._i;
     if (input === undefined) {
-        config._d = new Date(Moment.prototype.now());
+        config._d = new Date(hooks.now());
     } else if (isDate(input)) {
         config._d = new Date(+input);
     } else if (typeof input === 'string') {

src/lib/create/from-array.js
@@ -1,14 +1,15 @@
+import { hooks } from '../utils/hooks';
 import { createDate, createUTCDate } from './date-from-array';
 import { daysInYear } from '../units/year';
 import { weekOfYear, weeksInYear, dayOfYearFromWeeks } from '../units/week-calendar-utils';
 import { YEAR, MONTH, DATE, HOUR, MINUTE, SECOND, MILLISECOND } from '../units/constants';
 import { createLocal } from './local';
-import { Moment } from '../moment/constructor';
 import defaults from '../utils/defaults';
 import getParsingFlags from './parsing-flags';
 
 function currentDateArray(config) {
-    var nowValue = new Date(Moment.prototype.now());
+    // hooks is actually the exported moment object
+    var nowValue = new Date(hooks.now());
     if (config._useUTC) {
         return [nowValue.getUTCFullYear(), nowValue.getUTCMonth(), nowValue.getUTCDate()];
     }

src/lib/moment/prototype.js
@@ -13,7 +13,6 @@ import { to, toNow } from './to';
 import { getSet } from './get-set';
 import { locale, localeData, lang } from './locale';
 import { prototypeMin, prototypeMax } from './min-max';
-import { now } from './now';
 import { startOf, endOf } from './start-end-of';
 import { valueOf, toDate, toArray, toObject, toJSON, unix } from './to-type';
 import { isValid, parsingFlags, invalidAt } from './valid';
@@ -43,7 +42,6 @@ proto.locale            = locale;
 proto.localeData        = localeData;
 proto.max               = prototypeMax;
 proto.min               = prototypeMin;
-proto.now               = now;
 proto.parsingFlags      = parsingFlags;
 proto.set               = getSet;
 proto.startOf           = startOf;

commit 0e49f616f4b3f2dc24eeb1ae7d6af9a153433300

Fix Date mocking regression introduced in 2.11.0
The commit https://github.com/moment/moment/commit/f54a32f4c62c12a710335211a73cb002a7249646 and the follow on refactoring https://github.com/moment/moment/commit/b1d686ed481e2211cb81a1c60eca850fcea93f21 broke the ability for Date mocking libraries ([MockDate](https://www.npmjs.com/package/mockdate), SinonJS, etc.) to mock out the Date object. I believe we should restore this functionality.

```js
const moment = require('moment'),
    mockdate = require('mockdate'),
    frozenTime = moment();

mockdate.set(frozenTime);
console.log('Frozen time: ', frozenTime.toISOString());

setTimeout(function() {
    console.log('Moment time: ', moment().toISOString());
}, 1000);
```

Running the code above will yield the following in `moment@2.10.6` and `moment@2.11.0` respectively.

```bash
$ node index.js
Frozen time:  2016-01-04T14:21:55.335Z
Moment time:  2016-01-04T14:21:55.335Z

$ node index.js
Frozen time:  2016-01-04T14:21:55.335Z
Moment time:  2016-01-04T14:21:56.355Z
```

- Fixes #2857
- Fixes #2855

src/lib/moment/now.js
@@ -1,3 +1,3 @@
-export var now = Date.now || function () {
-    return +(new Date());
+export var now = function () {
+    return Date.now ? Date.now() : +(new Date());
 };

src/test/moment/now.js
@@ -12,6 +12,31 @@ test('now', function (assert) {
     assert.ok(momentNowTime <= afterMomentCreationTime, 'moment now() time should be now, not in the future');
 });
 
+test('now - Date mocked', function (assert) {
+    // We need to test mocking the global Date object, so disable 'Read Only' jshint check
+    /* jshint -W020 */
+    var RealDate = Date,
+        customTimeStr = '2015-01-01T01:30:00.000Z';
+
+    function MockDate() {
+        return new RealDate(customTimeStr);
+    }
+
+    MockDate.now = function () {
+        return new MockDate().valueOf();
+    };
+
+    MockDate.prototype = RealDate.prototype;
+
+    Date = MockDate;
+
+    try {
+        assert.ok(moment().toISOString() === customTimeStr, 'moment now() time should use the global Date object');
+    } finally {
+        Date = RealDate;
+    }
+});
+
 test('now - custom value', function (assert) {
     var customTimeStr = '2015-01-01T01:30:00.000Z',
         customTime = moment(customTimeStr, moment.ISO_8601).valueOf(),