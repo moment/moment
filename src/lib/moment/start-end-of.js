bcbd728f64a5e711603ddcf32002ca6215568720
Arturo Coronel <aoitsu3@gmail.com>
2016-02-22 20:25:13 -0800

    Add 'date' as alias to 'day' for startOf() and endOf()

--- src/lib/moment/start-end-of.js
+++ src/lib/moment/start-end-of.js
@@ -15,6 +15,7 @@ export function startOf (units) {
     case 'week':
     case 'isoWeek':
     case 'day':
+    case 'date':
         this.hours(0);
         /* falls through */
     case 'hour':
@@ -48,5 +49,11 @@ export function endOf (units) {
     if (units === undefined || units === 'millisecond') {
         return this;
     }
+
+    // 'date' is an alias for 'day', so it should be considered as such.
+    if (units === 'date') {
+        units = 'day';
+    }
+
     return this.startOf(units).add(1, (units === 'isoWeek' ? 'week' : units)).subtract(1, 'ms');
 }
