e1c1fb44ec22538d727e18f8c051da01f369c2e3
David Mai <david.mai@isocket.com>
2016-02-22 17:52:56 -0800

    accurate assertion descriptions

--- src/test/moment/weeks.js
+++ src/test/moment/weeks.js
@@ -119,11 +119,11 @@ test('weeks setter', function (assert) {
 });
 
 test('weekOfMonth', function (assert) {
-    assert.equal(moment([2012, 0,  1]).weekOfMonth(), 1, 'Setting Jan 1 2012 to week 30 should work');
-    assert.equal(moment([2012, 0,  7]).weekOfMonth(), 1, 'Setting Jan 7 2012 to week 30 should work');
-    assert.equal(moment([2012, 0,  8]).weekOfMonth(), 2, 'Setting Jan 8 2012 to week 30 should work');
-    assert.equal(moment([2012, 0, 14]).weekOfMonth(), 2, 'Setting Jan 14 2012 to week 30 should work');
-    assert.equal(moment([2012, 0, 15]).weekOfMonth(), 3, 'Setting Jan 15 2012 to week 30 should work');
+    assert.equal(moment([2012, 0,  1]).weekOfMonth(), 1, 'Jan 1 is in the first week of January');
+    assert.equal(moment([2012, 0,  7]).weekOfMonth(), 1, 'Jan 7 is also in the first week of January');
+    assert.equal(moment([2012, 0,  8]).weekOfMonth(), 2, 'Jan 8 is in the second week of January');
+    assert.equal(moment([2012, 0, 14]).weekOfMonth(), 2, 'Jan 14 is in the second week of January');
+    assert.equal(moment([2012, 3, 15]).weekOfMonth(), 3, 'April 15 is in the second week of April');
 });
 
 test('iso weeks setter', function (assert) {
