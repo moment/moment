220b372ccbe18c2defecb1860adafd226dcdd68e
Matt Johnson <mj1856@hotmail.com>
2016-03-07 20:40:41 -0800

    Suppress deprecation warnings in unit tests

--- src/test/moment/deprecate.js
+++ src/test/moment/deprecate.js
@@ -1,11 +1,13 @@
 import { module, test, expect } from '../qunit';
 import { deprecate } from '../../lib/utils/deprecate';
+import { hooks } from '../../lib/utils/hooks';
 import moment from '../../moment';
 
 module('deprecate');
 
 test('deprecate', function (assert) {
     var fn = function () {};
+    hooks.suppressDeprecationWarnings = true;
     var deprecatedFn = deprecate('testing deprecation', fn);
     deprecatedFn();
 
--- src/test/qunit.js
+++ src/test/qunit.js
@@ -10,6 +10,7 @@ export var expect = QUnit.expect;
 export function module (name, lifecycle) {
     QUnit.module(name, {
         setup : function () {
+            moment.suppressDeprecationWarnings = true;
             moment.locale('en');
             moment.createFromInputFallback = function (config) {
                 throw new Error('input not handled by moment: ' + config._i);
@@ -29,6 +30,7 @@ export function module (name, lifecycle) {
 export function localeModule (name, lifecycle) {
     QUnit.module('locale:' + name, {
         setup : function () {
+            moment.suppressDeprecationWarnings = true;
             moment.locale(name);
             moment.createFromInputFallback = function (config) {
                 throw new Error('input not handled by moment: ' + config._i);
