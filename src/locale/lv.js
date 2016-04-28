d7a402b0b99009130828cf599ba5696eb9504e03
Kārlis Gaņģis <karlis@zoomcharts.com>
2016-04-28 16:05:06 +0300

    Fixed Latvian locale - 'After 211 years'

--- src/locale/lv.js
+++ src/locale/lv.js
@@ -23,11 +23,11 @@ var units = {
 function format(forms, number, withoutSuffix) {
     if (withoutSuffix) {
         // E.g. "21 minūte", "3 minūtes".
-        return number % 10 === 1 && number !== 11 ? forms[2] : forms[3];
+        return number % 10 === 1 && number % 100 !== 11 ? forms[2] : forms[3];
     } else {
         // E.g. "21 minūtes" as in "pēc 21 minūtes".
         // E.g. "3 minūtēm" as in "pēc 3 minūtēm".
-        return number % 10 === 1 && number !== 11 ? forms[0] : forms[1];
+        return number % 10 === 1 && number % 100 !== 11 ? forms[0] : forms[1];
     }
 }
 function relativeTimeWithPlural(number, withoutSuffix, key) {
