be13fcd6ad914276cfdf0aa7f52bf2a5eaa9c1fd
Jonathan Gray <jonathan.sam.gray@gmail.com>
2016-05-21 13:06:10 -0700

    toDate: remove ternary, add unit test

--- src/lib/moment/to-type.js
+++ src/lib/moment/to-type.js
@@ -7,7 +7,7 @@ export function unix () {
 }
 
 export function toDate () {
-    return this._offset ? new Date(this.valueOf()) : new Date(this._d);
+    return new Date(this.valueOf());
 }
 
 export function toArray () {
