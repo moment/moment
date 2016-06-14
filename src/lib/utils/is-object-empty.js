76fea370307d7091b06e8a34d4a6c219fdb87dae
Iskren Chernev <iskren.chernev@gmail.com>
2016-06-14 01:48:01 -0700

    Handle empty object and empty array for creation as now
    Redo #3111, fix #3085

---
+++ src/lib/utils/is-object-empty.js
@@ -0,0 +1,8 @@
+export default function isObjectEmpty(obj) {
+    var k;
+    for (k in obj) {
+        // even if its not own property I'd still call it non-empty
+        return false;
+    }
+    return true;
+}
