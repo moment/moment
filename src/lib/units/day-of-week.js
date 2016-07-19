fc2b366eea1c96715f58907f43d471d09e336286
Siben Nayak <siben.nayak@gmail.com>
2016-07-19 13:27:04 +0530

    Fix for locale-data without moment (fixes #3284)

--- src/lib/units/day-of-week.js
+++ src/lib/units/day-of-week.js
@@ -99,17 +99,26 @@ function parseIsoWeekday(input, locale) {
 
 export var defaultLocaleWeekdays = 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_');
 export function localeWeekdays (m, format) {
+    if (typeof m === 'undefined') {
+        return this._weekdays;
+    }
     return isArray(this._weekdays) ? this._weekdays[m.day()] :
         this._weekdays[this._weekdays.isFormat.test(format) ? 'format' : 'standalone'][m.day()];
 }
 
 export var defaultLocaleWeekdaysShort = 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_');
 export function localeWeekdaysShort (m) {
+    if (typeof m === 'undefined') {
+        return this._weekdaysShort;
+    }
     return this._weekdaysShort[m.day()];
 }
 
 export var defaultLocaleWeekdaysMin = 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_');
 export function localeWeekdaysMin (m) {
+    if (typeof m === 'undefined') {
+        return this._weekdaysMin;
+    }
     return this._weekdaysMin[m.day()];
 }
 
