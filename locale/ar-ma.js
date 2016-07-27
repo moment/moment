929aa8f4d7274d564699ad535df9ead4190de48c
Khaled Said <khaled@shebak.com>
2016-07-27 12:01:00 +0200

    fixing sequence of Arabic days

--- locale/ar-ma.js
+++ locale/ar-ma.js
@@ -50,7 +50,7 @@
             yy : '%d سنوات'
         },
         week : {
-            dow : 6, // Saturday is the first day of the week.
+            dow : 1, // Saturday is the first day of the week.
             doy : 12  // The week that contains Jan 1st is the first week of the year.
         }
     });
--- locale/ar.js
+++ locale/ar.js
@@ -127,7 +127,7 @@
             }).replace(/,/g, '،');
         },
         week : {
-            dow : 6, // Saturday is the first day of the week.
+            dow : 1, // Saturday is the first day of the week.
             doy : 12  // The week that contains Jan 1st is the first week of the year.
         }
     });
