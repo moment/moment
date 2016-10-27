cc9a07dc479f7b1acc50b4ad1d6ce1b424afc31a
Ben Newman <ben@meteor.com>
2016-10-27 11:48:54 -0700

    Publish Meteor package from meteor directory to fix #3531.

--- Gruntfile.js
+++ Gruntfile.js
@@ -159,24 +159,15 @@ module.exports = function (grunt) {
         },
         exec: {
             'meteor-init': {
-                command: [
-                    // Make sure Meteor is installed, per https://meteor.com/install.
-                    // The curl'ed script is safe; takes 2 minutes to read source & check.
-                    'type meteor >/dev/null 2>&1 || { curl https://install.meteor.com/ | sh; }',
-                    // Meteor expects package.js to be in the root directory of
-                    // the checkout, but we already have a package.js for Dojo
-                    'mv package.js package.dojo && cp meteor/package.js .'
-                ].join(';')
-            },
-            'meteor-cleanup': {
-                // remove build files and restore Dojo's package.js
-                command: 'rm -rf ".build.*" versions.json; mv package.dojo package.js'
+                // Make sure Meteor is installed, per https://meteor.com/install.
+                // The curl'ed script is safe; takes 2 minutes to read source & check.
+                command: 'type meteor >/dev/null 2>&1 || { curl https://install.meteor.com/ | sh; }'
             },
             'meteor-test': {
-                command: 'spacejam --mongo-url mongodb:// test-packages ./'
+                command: 'spacejam --mongo-url mongodb:// test-packages ./meteor'
             },
             'meteor-publish': {
-                command: 'meteor publish'
+                command: 'cd meteor && meteor publish'
             },
             'typescript-test': {
                 command: 'node_modules/.bin/tsc --project typing-tests'
