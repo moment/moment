35ee5a5b68e5080d122a2d167ae7d3827acc1de1
Kunal Marwaha <marwahaha@berkeley.edu>
2016-11-30 01:06:54 -0500

    Replace arrow function

--- src/lib/duration/constructor.js
+++ src/lib/duration/constructor.js
@@ -6,7 +6,9 @@ export function Duration (duration) {
     var normalizedInput = normalizeObjectUnits(duration);
 
     this._isValid = isDurationValid(normalizedInput);
-    this.isValid = () => this._isValid;
+    this.isValid = function () {
+        return this._isValid;
+    };
 
     var years = this._isValid && normalizedInput.year || 0,
         quarters = this._isValid && normalizedInput.quarter || 0,
