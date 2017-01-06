aa7aabc5725f92b073e5b6d324e6f52118c03c01
Will Dembinski <willdembinski@gmail.com>
2017-01-05 21:20:59 -0800

    Added semicolon to helper

--- src/lib/utils/is-numeric.js
+++ src/lib/utils/is-numeric.js
@@ -2,5 +2,5 @@ export default function isNumeric(val) {
     var _val = +val;
     return (val !== val + 1) && //infinity check
            ( _val === +val ) && //Cute coercion check
-           (typeof val !== 'object') //Array/object check
+           (typeof val !== 'object'); //Array/object check
 }
