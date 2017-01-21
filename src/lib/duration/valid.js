f95841e59373499e34aeec51c33d9c647c8334c8
Kunal Marwaha <marwahaha@berkeley.edu>
2017-01-20 21:23:29 -0500

    Add validity check for decimal values on non-smallest duration unit

--- src/lib/duration/valid.js
+++ src/lib/duration/valid.js
@@ -1,9 +1,24 @@
+var ordering = ['year', 'quarter', 'month', 'week', 'day', 'hour', 'minute', 'second', 'millisecond'];
+
 export default function isDurationValid(m) {
     for (var key in m) {
-        if (['year', 'quarter', 'month', 'week', 'day', 'hour', 'minute', 'second', 'millisecond'].indexOf(key) === -1 ||
+        if (ordering.indexOf(key) === -1 ||
         m[key] !== undefined && isNaN(parseInt(m[key]))) {
             return false;
         }
     }
+
+    var unitHasDecimal = false;
+    for (var i = 0; i < ordering.length; ++i) {
+        if (m[ordering[i]]) {
+            if (unitHasDecimal) {
+                return false; // only allow non-integers for smallest unit
+            }
+            if (parseFloat(m[ordering[i]]) !== parseInt(m[ordering[i]])) {
+                unitHasDecimal = true;
+            }
+        }
+    }
+
     return true;
 }
