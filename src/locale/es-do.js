25e5ec57c92439cc1a5e677da70837164d7608eb
Tim Fish <tim@timfishy.co.uk>
2017-03-04 16:14:27 +0100

    Fix code style errors

--- src/locale/es-do.js
+++ src/locale/es-do.js
@@ -9,7 +9,7 @@ var monthsShortDot = 'ene._feb._mar._abr._may._jun._jul._ago._sep._oct._nov._dic
 export default moment.defineLocale('es-do', {
     months : 'enero_febrero_marzo_abril_mayo_junio_julio_agosto_septiembre_octubre_noviembre_diciembre'.split('_'),
     monthsShort : function (m, format) {
-        if(!m){
+        if (!m) {
             return monthsShortDot;
         } else if (/-MMM-/.test(format)) {
             return monthsShort[m.month()];
