d96bbeccc763c4d6537fe0ac75763fcb6f4c86c3
Kunal Marwaha <marwahaha@berkeley.edu>
2017-03-29 02:35:52 -0400

    Calculate daysInMonth without Date()

--- src/lib/units/year.js
+++ src/lib/units/year.js
@@ -56,7 +56,7 @@ export function daysInYear(year) {
     return isLeapYear(year) ? 366 : 365;
 }
 
-function isLeapYear(year) {
+export function isLeapYear(year) {
     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
