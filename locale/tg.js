51dac1274cd3d003439e5600f750dfe5d1d8e3a6
Orif N. Jr <orif.zade@gmail.com>
2017-04-05 22:15:15 +0200

    add Tajik (cyrillic) language

---
+++ locale/tg.js
@@ -0,0 +1,58 @@
+//! moment.js locale configuration
+//! locale : Tajik [tg]
+//! author : Orif N. Jr. : https://github.com/orif-jr
+
+;(function (global, factory) {
+   typeof exports === 'object' && typeof module !== 'undefined'
+       && typeof require === 'function' ? factory(require('../moment')) :
+   typeof define === 'function' && define.amd ? define(['../moment'], factory) :
+   factory(global.moment)
+}(this, (function (moment) { 'use strict';
+
+
+var tg = moment.defineLocale('tg', {
+    months : 'январ_феврал_март_апрел_май_июн_июл_август_сентябр_октябр_ноябр_декабр'.split('_'),
+    monthsShort : 'янв_фев_мар_апр_май_июн_июл_авг_сен_окт_ноя_дек'.split('_'),
+    weekdays : 'Якшанбе_Душанбе_Сешанбе_Чоршанбе_Панҷшанбе_Ҷумъа_Шанбе'.split('_'),
+    weekdaysShort : 'Яшб_Дшб_Сшб_Чшб_Пшб_Ҷум_Шан'.split('_'),
+    weekdaysMin : 'Яш_Дш_Сш_Чш_Пш_Ҷм_Шб'.split('_'),
+    longDateFormat : {
+        LT : 'HH:mm',
+        LTS : 'HH:mm:ss',
+        L : 'DD/MM/YYYY',
+        LL : 'D MMMM YYYY',
+        LLL : 'D MMMM YYYY HH:mm',
+        LLLL : 'D MMMM YYYY, dddd HH:mm'
+    },
+    calendar : {
+        sameDay : '[Имрӯз соати] LT',
+        nextDay : '[Пагоҳ соати] LT',
+        lastDay : '[Дирӯз соати] LT',
+        nextWeek : 'dddd [оянда соати] LT',
+        lastWeek : 'dddd [гузашта соати] LT',
+        sameElse : 'L'
+    },
+    relativeTime : {
+        future : 'Баъди %s',
+        past : '%s пеш',
+        s : 'чанд сония',
+        m : 'як дақиқа',
+        mm : '%d дақиқа',
+        h : 'як соат',
+        hh : '%d соат',
+        d : 'як рӯз',
+        dd : '%d рӯз',
+        M : 'як моҳ',
+        MM : '%d моҳ',
+        y : 'як сол',
+        yy : '%d сол'
+    },
+    week : {
+        dow : 1, // Monday is the first day of the week.
+        doy : 7  // The week that contains Jan 4th is the first week of the year.
+    }
+});
+
+return tg;
+
+})));
