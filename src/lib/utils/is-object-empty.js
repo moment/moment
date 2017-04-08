a603618e9927e5d405b2dc0a5abfb49351322ced
sgorsh <simon@gorse.org>
2017-04-08 23:34:39 +0200

    Use Object.keys for empty object check - improves performance.

--- src/lib/utils/is-object-empty.js
+++ src/lib/utils/is-object-empty.js
@@ -1,8 +1,12 @@
 export default function isObjectEmpty(obj) {
-    var k;
-    for (k in obj) {
-        // even if its not own property I'd still call it non-empty
-        return false;
+    if (Object.keys) {
+        return (Object.keys(obj).length === 0);
+    } else {
+        var k;
+        for (k in obj) {
+            // even if its not own property I'd still call it non-empty
+            return false;
+        }
+        return true;
     }
-    return true;
 }
