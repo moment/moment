ac8e90fefd700fe877585bbd54ec30991236fdf1
Gerardo Buenrostro González <blogcraft@users.noreply.github.com>
2017-04-11 16:29:04 -0300

    Update cs.js
    Unnecessary break in switch causes "Unreachable 'break' after 'return'." warning with JSLint

--- src/locale/cs.js
+++ src/locale/cs.js
@@ -22,7 +22,6 @@ function translate(number, withoutSuffix, key, isFuture) {
             } else {
                 return result + 'minutami';
             }
-            break;
         case 'h':  // an hour / in an hour / an hour ago
             return withoutSuffix ? 'hodina' : (isFuture ? 'hodinu' : 'hodinou');
         case 'hh': // 9 hours / in 9 hours / 9 hours ago
@@ -31,7 +30,6 @@ function translate(number, withoutSuffix, key, isFuture) {
             } else {
                 return result + 'hodinami';
             }
-            break;
         case 'd':  // a day / in a day / a day ago
             return (withoutSuffix || isFuture) ? 'den' : 'dnem';
         case 'dd': // 9 days / in 9 days / 9 days ago
@@ -40,7 +38,6 @@ function translate(number, withoutSuffix, key, isFuture) {
             } else {
                 return result + 'dny';
             }
-            break;
         case 'M':  // a month / in a month / a month ago
             return (withoutSuffix || isFuture) ? 'měsíc' : 'měsícem';
         case 'MM': // 9 months / in 9 months / 9 months ago
@@ -49,7 +46,6 @@ function translate(number, withoutSuffix, key, isFuture) {
             } else {
                 return result + 'měsíci';
             }
-            break;
         case 'y':  // a year / in a year / a year ago
             return (withoutSuffix || isFuture) ? 'rok' : 'rokem';
         case 'yy': // 9 years / in 9 years / 9 years ago
@@ -58,7 +54,6 @@ function translate(number, withoutSuffix, key, isFuture) {
             } else {
                 return result + 'lety';
             }
-            break;
     }
 }
 
