4d1053c3ceb345b30ab07c326ef08a5a9645ce8b
kostas <kkopanidis@gmail.com>
2017-05-18 16:50:59 +0300

    - Changed test

--- src/test/locale/es-do.js
+++ src/test/locale/es-do.js
@@ -212,7 +212,7 @@ test('weeks year starting sunday formatted', function (assert) {
 });
 
 test('test short months proper', function (assert) {
-    var str = "02-ago-2016"; // "02-ago-2016"
+    var str = '02-ago-2016'; // "02-ago-2016"
     assert.equal(moment(str, 'DD-MMM-YYYY').month(), '7', '02-ago-2016 month should be 7');
 });
 
