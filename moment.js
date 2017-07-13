b1d62f874b20d78af4298b32d337646143633fd0
andy-shi88 <shi88.andy@gmail.com>
2017-07-13 21:33:33 +0700

    #4024 optimize code

--- moment.js
+++ moment.js
@@ -561,11 +561,7 @@ function set$1 (mom, unit, value) {
         * if: month == february && date == 29
         * then: set the date to 28
         */
-        if(unit === 'FullYear') {
-            if(mom._d.getMonth() === 1 && mom._d.getDate() === 29 ) {
-                mom._d.setDate(28);
-            }
-        }
+        unit === 'FullYear' && mom._d.getMonth() === 1 && mom._d.getDate() === 29 && mom._d.setDate(28);
         mom._d['set' + (mom._isUTC ? 'UTC' : '') + unit](value);
     }
 }
