c2d6135976c76c70ce47310c2837d0f7a3092119
asulaiman <markie_boy210@hotmail.com>
2017-07-26 13:15:36 -0400

    added missing semicolon

--- src/lib/locale/locales.js
+++ src/lib/locale/locales.js
@@ -80,7 +80,7 @@ export function getSetGlobalLocale (key, values) {
         }
         else {
             //warn user if arguments are passed but the locale could not be set
-            console.warn('Locale ' + key +  'not found. Did you forget to load it?')
+            console.warn('Locale ' + key +  'not found. Did you forget to load it?');
         }
     }
 
