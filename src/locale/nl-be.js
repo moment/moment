12b6e5a96c0c4fae9b33688d35d5726f05c0cf05
Stephan <stephan-v@hotmail.com>
2017-08-03 04:21:26 +0200

    NL and BE locale weekDaysMin should not be capitalized, updated to lowercase. (#3961)
    * NL and BE locale weekDaysMin should not be capitalized, updated to lowercase
    
    * Updated locale nl and be weekdays with lowercase

--- src/locale/nl-be.js
+++ src/locale/nl-be.js
@@ -34,7 +34,7 @@ export default moment.defineLocale('nl-be', {
 
     weekdays : 'zondag_maandag_dinsdag_woensdag_donderdag_vrijdag_zaterdag'.split('_'),
     weekdaysShort : 'zo._ma._di._wo._do._vr._za.'.split('_'),
-    weekdaysMin : 'Zo_Ma_Di_Wo_Do_Vr_Za'.split('_'),
+    weekdaysMin : 'zo_ma_di_wo_do_vr_za'.split('_'),
     weekdaysParseExact : true,
     longDateFormat : {
         LT : 'HH:mm',
