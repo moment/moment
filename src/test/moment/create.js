8700e65aa9fc251520972a0da9f2296c8a2adc9a
Mike Paras <mikeparas@gmail.com>
2017-08-19 10:43:22 -0700

    Update tests for mismatching weekday
    * Add test to parsing_flags test module
    * Reword assertion messages

--- src/test/moment/create.js
+++ src/test/moment/create.js
@@ -1225,7 +1225,8 @@ test('k, kk', function (assert) {
 });
 
 test('mismatching day-of-week and date', function (assert) {
-    assert.ok(!moment('Wed 08-10-2017', 'ddd MM-DD-YYYY').isValid(), 'because the day-of-the-week is incorrect for the date');
-    assert.ok(moment('Thu 08-10-2017', 'ddd MM-DD-YYYY').isValid(), 'because the day-of-the-week is correct for the date');
+    // string with format
+    assert.ok(!moment('Wed 08-10-2017', 'ddd MM-DD-YYYY').isValid(), 'because day of week is incorrect for the date');
+    assert.ok(moment('Thu 08-10-2017', 'ddd MM-DD-YYYY').isValid(), 'because day of week is correct for the date');
 });
 
