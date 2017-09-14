f2f97ebf6484beac1db6facb4c6f976e8851b181
Huáng Jùnliàng <i@jhuang.me>
2017-09-14 13:33:28 +0800

    add relativeSeconds for es-*

--- src/locale/es-do.js
+++ src/locale/es-do.js
@@ -61,6 +61,7 @@ export default moment.defineLocale('es-do', {
         future : 'en %s',
         past : 'hace %s',
         s : 'unos segundos',
+        ss : '%d segundos',
         m : 'un minuto',
         mm : '%d minutos',
         h : 'una hora',
