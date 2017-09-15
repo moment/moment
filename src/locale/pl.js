17214ac5eb00ba38ed9991478cc00bbb4fda5fd8
Huáng Jùnliàng <i@jhuang.me>
2017-09-16 00:19:41 +0800

    fix pl

--- src/locale/pl.js
+++ src/locale/pl.js
@@ -12,6 +12,8 @@ function plural(n) {
 function translate(number, withoutSuffix, key) {
     var result = number + ' ';
     switch (key) {
+        case 'ss':
+            return result + (plural(number) ? 'sekundy' : 'sekund');
         case 'm':
             return withoutSuffix ? 'minuta' : 'minutę';
         case 'mm':
@@ -94,7 +96,7 @@ export default moment.defineLocale('pl', {
         future : 'za %s',
         past : '%s temu',
         s : 'kilka sekund',
-        ss : '%d sekund'
+        ss : translate,
         m : translate,
         mm : translate,
         h : translate,
