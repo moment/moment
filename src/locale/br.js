a634e939c3339a7505470651d6118900bd0a5b59
Huáng Jùnliàng <i@jhuang.me>
2017-09-16 22:47:43 +0800

    br

--- src/locale/br.js
+++ src/locale/br.js
@@ -75,6 +75,7 @@ export default moment.defineLocale('br', {
         future : 'a-benn %s',
         past : '%s \'zo',
         s : 'un nebeud segondennoù',
+        ss : '%d eilenn',
         m : 'ur vunutenn',
         mm : relativeTimeWithMutation,
         h : 'un eur',
