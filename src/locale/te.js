2c9e10272ef4283d9377bf7edbac4a4a46113139
Huáng Jùnliàng <i@jhuang.me>
2017-09-20 23:29:19 +0800

    fix te from CLDR

--- src/locale/te.js
+++ src/locale/te.js
@@ -31,7 +31,7 @@ export default moment.defineLocale('te', {
         future : '%s లో',
         past : '%s క్రితం',
         s : 'కొన్ని క్షణాలు',
-        ss : '%d సెకండ్స్',
+        ss : '%d సెకన్లు',
         m : 'ఒక నిమిషం',
         mm : '%d నిమిషాలు',
         h : 'ఒక గంట',
