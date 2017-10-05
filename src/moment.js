54d7e19b8064fb6516743fcbc7be3204a4ec65ba
Tom Clarkson <tom@orangeguava.com>
2017-10-06 11:17:05 +1300

    Earlier refactoring removed the need for #4042

--- src/moment.js
+++ src/moment.js
@@ -6,15 +6,8 @@ var hackedRequireToIgnoreError = require;
 
 setLocaleLoader(function (name) {
     try {
-        hackedRequireToIgnoreError('moment/locale/' + name);
-    } catch (e) {
-        // In the test environment, the external module 'moment'
-        // can't be resolved because we're running inside it.
-        // Fallback to using the old relative import
-        try {
-            hackedRequireToIgnoreError('./locale/' + name);
-        } catch (e) { }
-    }
+        hackedRequireToIgnoreError('./locale/' + name);
+    } catch (e) { }
 });
 
 export default moment;
