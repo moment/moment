a8749bc092b1a9b67611c5da917b13a013cdbf84
Bardur Pihl <bhp@trustpilot.com>
2017-12-14 16:17:46 +0100

    define amd module to moment
    if define.amd is available then currently the moment library is defined as an anonymous factory.
    moment needs to be named if it should be used in other modules.

--- moment.js
+++ moment.js
@@ -6,7 +6,7 @@
 
 ;(function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
+    typeof define === 'function' && define.amd ? define('moment', factory) :
     global.moment = factory()
 }(this, (function () { 'use strict';
 
