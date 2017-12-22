fe1d71f82e0140ff2f45f9976af176da7e887ca7
marcosvega91 <marcomoretti0103@gmail.com>
2017-12-22 20:38:30 +0100

    Fixed lint errors

--- src/test/moment/to.js
+++ src/test/moment/to.js
@@ -9,11 +9,8 @@ const tomorrow = moment('2017-08-17T00:02:01.249Z');
 
 
 test('Expected to be one month ago', function (assert) {
-
-
-    assert.ok(now.to(expiredAt)==="a month ago");
-    assert.ok(tomorrow.to(expiredAt)==="a month ago");
-
+    assert.ok(now.to(expiredAt) === 'a month ago');
+    assert.ok(tomorrow.to(expiredAt) === 'a month ago');
     moment.relativeTimeThreshold('d', 180);
     now.to(expiredAt);
     tomorrow.to(expiredAt);
@@ -22,21 +19,18 @@ test('Expected to be one month ago', function (assert) {
 
 test('Expected to be 30 days ago', function (assert) {
     moment.relativeTimeThreshold('d', 180);
-
-    assert.ok(now.to(expiredAt)==="30 days ago");
-
+    assert.ok(now.to(expiredAt) === '30 days ago');
 });
 
 
 test('Expected to be 31 days ago', function (assert) {
     moment.relativeTimeThreshold('d', 180);
 
-    assert.ok(tomorrow.to(expiredAt)==="31 days ago");
+    assert.ok(tomorrow.to(expiredAt) === '31 days ago');
 });
 
-test('Expected to be 34 years ago',function(assert){
-    const to = moment("1983-09-20");
-    const from = moment("2017-08-23");
-    assert.ok(from.to(to)==='34 years ago');
-
+test('Expected to be 34 years ago',function (assert) {
+    const to = moment('1983-09-20');
+    const from = moment('2017-08-23');
+    assert.ok(from.to(to) === '34 years ago');
 });
