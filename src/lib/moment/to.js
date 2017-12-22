d7e572422ebb9d06bd67b5746e72889d1a5bcb57
marcosvega91 <marcomoretti0103@gmail.com>
2017-12-22 12:09:56 +0100

    [bugfix] Fix #4127
    I have fixed the issue that doesn’t show the right result as described in #4127 issue.
    I have followed the suggestion of @nekitk  and changed the to method. I also added some unit test

--- src/lib/moment/to.js
+++ src/lib/moment/to.js
@@ -1,12 +1,12 @@
-import { createDuration } from '../duration/create';
 import { createLocal } from '../create/local';
 import { isMoment } from '../moment/constructor';
+import { createDuration } from '../duration/create';
 
 export function to (time, withoutSuffix) {
     if (this.isValid() &&
             ((isMoment(time) && time.isValid()) ||
              createLocal(time).isValid())) {
-        return createDuration({from: this, to: time}).locale(this.locale()).humanize(!withoutSuffix);
+        return createDuration(time.diff(this)).locale(this.locale()).humanize(!withoutSuffix);
     } else {
         return this.localeData().invalidDate();
     }
---
+++ src/test/moment/to.js
@@ -0,0 +1,42 @@
+import { module, test, expect } from '../qunit';
+import moment from '../../moment';
+module('to');
+
+
+const expiredAt = moment('2017-07-16T21:01:54.249Z');
+const now = moment('2017-08-16T00:02:01.249Z');
+const tomorrow = moment('2017-08-17T00:02:01.249Z');
+
+
+test('Expected to be one month ago', function (assert) {
+
+
+    assert.ok(now.to(expiredAt)==="a month ago");
+    assert.ok(tomorrow.to(expiredAt)==="a month ago");
+
+    moment.relativeTimeThreshold('d', 180);
+    now.to(expiredAt);
+    tomorrow.to(expiredAt);
+});
+
+
+test('Expected to be 30 days ago', function (assert) {
+    moment.relativeTimeThreshold('d', 180);
+
+    assert.ok(now.to(expiredAt)==="30 days ago");
+
+});
+
+
+test('Expected to be 31 days ago', function (assert) {
+    moment.relativeTimeThreshold('d', 180);
+
+    assert.ok(tomorrow.to(expiredAt)==="31 days ago");
+});
+
+test('Expected to be 34 years ago',function(assert){
+    const to = moment("1983-09-20");
+    const from = moment("2017-08-23");
+    assert.ok(from.to(to)==='34 years ago');
+
+});
