42f1c3f2a808ac7ce8e0e7bd8407de698124e688
Bardur Pihl <bhp@trustpilot.com>
2018-01-04 20:55:51 +0100

    define module as moment

--- templates/default.js
+++ templates/default.js
@@ -1,5 +1,5 @@
 ;(function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
+    typeof define === 'function' && define.amd ? define('moment', factory) :
     global.moment = factory()
 }(this, (function () { 'use strict';
