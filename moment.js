c0c06375adbbb90b1eb627c879290c46835127ff
Bardur Pihl <bhp@trustpilot.com>
2018-01-04 20:53:24 +0100

    Revert change to auto-generated file

--- moment.js
+++ moment.js
@@ -6,7 +6,7 @@
 
 ;(function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define('moment', factory) :
+    typeof define === 'function' && define.amd ? define(factory) :
     global.moment = factory()
 }(this, (function () { 'use strict';
 
