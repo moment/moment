3a33f37103b733cb91c2f72be649a5f43e08e5bf
Ash Searle <ash@hexmen.com>
2018-01-04 11:17:14 +0000

    Add min & max typescript tests

--- typing-tests/moment-tests.ts
+++ typing-tests/moment-tests.ts
@@ -200,6 +200,9 @@ a8.diff(b8, 'days');
 a8.diff(b8, 'years')
 a8.diff(b8, 'years', true);
 
+moment.min(a8, b8);
+moment.max(a8, b8);
+
 moment([2007, 0, 29]).toDate();
 moment([2007, 1, 23]).toISOString();
 moment(1318874398806).valueOf();
