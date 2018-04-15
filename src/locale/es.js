47a4bbc3bd35cc3b8ecb9afe504ea353fc86fd71
Kunal Marwaha <marwahaha@berkeley.edu>
2018-04-14 22:36:59 -0700

    Fix lint

--- src/locale/es.js
+++ src/locale/es.js
@@ -80,5 +80,5 @@ export default moment.defineLocale('es', {
         dow : 1, // Monday is the first day of the week.
         doy : 4  // The week that contains Jan 4th is the first week of the year.
     },
-    invalidDate: "Fecha invalida"
+    invalidDate: 'Fecha invalida'
 });
