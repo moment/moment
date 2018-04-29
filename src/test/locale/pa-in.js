af77b3e869dd820571594bd5bbeb7cd7107fe610
Ash <ash@hexmen.com>
2018-04-29 21:42:28 +0100

    [locale] Fix #4568: Insert punjabi for "next" (#4583)

--- src/test/locale/pa-in.js
+++ src/test/locale/pa-in.js
@@ -165,11 +165,11 @@ test('calendar next week', function (assert) {
     var i, m;
     for (i = 2; i < 7; i++) {
         m = moment().add({d: i});
-        assert.equal(m.calendar(),       m.format('dddd[,] LT'),  'Today + ' + i + ' days current time');
+        assert.equal(m.calendar(),       m.format('[ਅਗਲਾ] dddd[,] LT'),  'Today + ' + i + ' days current time');
         m.hours(0).minutes(0).seconds(0).milliseconds(0);
-        assert.equal(m.calendar(),       m.format('dddd[,] LT'),  'Today + ' + i + ' days beginning of day');
+        assert.equal(m.calendar(),       m.format('[ਅਗਲਾ] dddd[,] LT'),  'Today + ' + i + ' days beginning of day');
         m.hours(23).minutes(59).seconds(59).milliseconds(999);
-        assert.equal(m.calendar(),       m.format('dddd[,] LT'),  'Today + ' + i + ' days end of day');
+        assert.equal(m.calendar(),       m.format('[ਅਗਲਾ] dddd[,] LT'),  'Today + ' + i + ' days end of day');
     }
 });
 
