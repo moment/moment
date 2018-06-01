9eb3f755e8dad5a6e591507eeb808b0d56d83273
Kunal Marwaha <marwahaha@berkeley.edu>
2018-05-31 23:44:21 -0700

    Bump version to 2.22.2

--- src/moment.js
+++ src/moment.js
@@ -1,12 +1,12 @@
 //! moment.js
-//! version : 2.22.1
+//! version : 2.22.2
 //! authors : Tim Wood, Iskren Chernev, Moment.js contributors
 //! license : MIT
 //! momentjs.com
 
 import { hooks as moment, setHookCallback } from './lib/utils/hooks';
 
-moment.version = '2.22.1';
+moment.version = '2.22.2';
 
 import {
     min,
