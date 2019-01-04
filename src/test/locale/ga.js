c59fe3dd11fcd52605a5f4b7796325b15b7548e2
André Silva <2493377+askpt@users.noreply.github.com>
2019-01-04 10:54:20 +0000

    Added some more tests to ga locale.

--- src/test/locale/ga.js
+++ src/test/locale/ga.js
@@ -36,79 +36,79 @@ test('parse', function (assert) {
     }
 });
 
-// test('format', function (assert) {
-//     var a = [
-//         ['dddd, MMMM Do YYYY, h:mm:ss a', 'Didòmhnaich, An Gearran 14mh 2010, 3:25:50 pm'],
-//         ['ddd, hA', 'Did, 3PM'],
-//         ['M Mo MM MMMM MMM', '2 2na 02 An Gearran Gear'],
-//         ['YYYY YY', '2010 10'],
-//         ['D Do DD', '14 14mh 14'],
-//         ['d do dddd ddd dd', '0 0mh Didòmhnaich Did Dò'],
-//         ['DDD DDDo DDDD', '45 45mh 045'],
-//         ['w wo ww', '6 6mh 06'],
-//         ['h hh', '3 03'],
-//         ['H HH', '15 15'],
-//         ['m mm', '25 25'],
-//         ['s ss', '50 50'],
-//         ['a A', 'pm PM'],
-//         ['[an] DDDo [latha den bhliadhna]', 'an 45mh latha den bhliadhna'],
-//         ['LTS', '15:25:50'],
-//         ['L', '14/02/2010'],
-//         ['LL', '14 An Gearran 2010'],
-//         ['LLL', '14 An Gearran 2010 15:25'],
-//         ['LLLL', 'Didòmhnaich, 14 An Gearran 2010 15:25'],
-//         ['l', '14/2/2010'],
-//         ['ll', '14 Gear 2010'],
-//         ['lll', '14 Gear 2010 15:25'],
-//         ['llll', 'Did, 14 Gear 2010 15:25']
-//     ],
-//     b = moment(new Date(2010, 1, 14, 15, 25, 50, 125)),
-//     i;
-//     for (i = 0; i < a.length; i++) {
-//         assert.equal(b.format(a[i][0]), a[i][1], a[i][0] + ' ---> ' + a[i][1]);
-//     }
-// });
+test('format', function (assert) {
+    var a = [
+        ['dddd, MMMM Do YYYY, h:mm:ss a', 'Dé Domhnaigh, Feabhra 14mh 2010, 3:25:50 pm'],
+        ['ddd, hA', 'Dom, 3PM'],
+        ['M Mo MM MMMM MMM', '2 2na 02 Feabhra Feab'],
+        ['YYYY YY', '2010 10'],
+        ['D Do DD', '14 14mh 14'],
+        ['d do dddd ddd dd', '0 0mh Dé Domhnaigh Dom Do'],
+        ['DDD DDDo DDDD', '45 45mh 045'],
+        ['w wo ww', '6 6mh 06'],
+        ['h hh', '3 03'],
+        ['H HH', '15 15'],
+        ['m mm', '25 25'],
+        ['s ss', '50 50'],
+        ['a A', 'pm PM'],
+        ['[an] DDDo [latha den bhliadhna]', 'an 45mh latha den bhliadhna'],
+        ['LTS', '15:25:50'],
+        ['L', '14/02/2010'],
+        ['LL', '14 Feabhra 2010'],
+        ['LLL', '14 Feabhra 2010 15:25'],
+        ['LLLL', 'Dé Domhnaigh, 14 Feabhra 2010 15:25'],
+        ['l', '14/2/2010'],
+        ['ll', '14 Feab 2010'],
+        ['lll', '14 Feab 2010 15:25'],
+        ['llll', 'Dom, 14 Feab 2010 15:25']
+    ],
+    b = moment(new Date(2010, 1, 14, 15, 25, 50, 125)),
+    i;
+    for (i = 0; i < a.length; i++) {
+        assert.equal(b.format(a[i][0]), a[i][1], a[i][0] + ' ---> ' + a[i][1]);
+    }
+});
 
-// test('format ordinal', function (assert) {
-//     assert.equal(moment([2011, 0, 1]).format('DDDo'), '1d', '1d');
-//     assert.equal(moment([2011, 0, 2]).format('DDDo'), '2na', '2na');
-//     assert.equal(moment([2011, 0, 3]).format('DDDo'), '3mh', '3mh');
-//     assert.equal(moment([2011, 0, 4]).format('DDDo'), '4mh', '4mh');
-//     assert.equal(moment([2011, 0, 5]).format('DDDo'), '5mh', '5mh');
-//     assert.equal(moment([2011, 0, 6]).format('DDDo'), '6mh', '6mh');
-//     assert.equal(moment([2011, 0, 7]).format('DDDo'), '7mh', '7mh');
-//     assert.equal(moment([2011, 0, 8]).format('DDDo'), '8mh', '8mh');
-//     assert.equal(moment([2011, 0, 9]).format('DDDo'), '9mh', '9mh');
-//     assert.equal(moment([2011, 0, 10]).format('DDDo'), '10mh', '10mh');
-//     assert.equal(moment([2011, 0, 11]).format('DDDo'), '11mh', '11mh');
-//     assert.equal(moment([2011, 0, 12]).format('DDDo'), '12na', '12na');
-//     assert.equal(moment([2011, 0, 13]).format('DDDo'), '13mh', '13mh');
-//     assert.equal(moment([2011, 0, 14]).format('DDDo'), '14mh', '14mh');
-//     assert.equal(moment([2011, 0, 15]).format('DDDo'), '15mh', '15mh');
-//     assert.equal(moment([2011, 0, 16]).format('DDDo'), '16mh', '16mh');
-//     assert.equal(moment([2011, 0, 17]).format('DDDo'), '17mh', '17mh');
-//     assert.equal(moment([2011, 0, 18]).format('DDDo'), '18mh', '18mh');
-//     assert.equal(moment([2011, 0, 19]).format('DDDo'), '19mh', '19mh');
-//     assert.equal(moment([2011, 0, 20]).format('DDDo'), '20mh', '20mh');
-//     assert.equal(moment([2011, 0, 21]).format('DDDo'), '21mh', '21mh');
-//     assert.equal(moment([2011, 0, 22]).format('DDDo'), '22na', '22na');
-//     assert.equal(moment([2011, 0, 23]).format('DDDo'), '23mh', '23mh');
-//     assert.equal(moment([2011, 0, 24]).format('DDDo'), '24mh', '24mh');
-//     assert.equal(moment([2011, 0, 25]).format('DDDo'), '25mh', '25mh');
-//     assert.equal(moment([2011, 0, 26]).format('DDDo'), '26mh', '26mh');
-//     assert.equal(moment([2011, 0, 27]).format('DDDo'), '27mh', '27mh');
-//     assert.equal(moment([2011, 0, 28]).format('DDDo'), '28mh', '28mh');
-//     assert.equal(moment([2011, 0, 29]).format('DDDo'), '29mh', '29mh');
-//     assert.equal(moment([2011, 0, 30]).format('DDDo'), '30mh', '30mh');
-//     assert.equal(moment([2011, 0, 31]).format('DDDo'), '31mh', '31mh');
-// });
+test('format ordinal', function (assert) {
+    assert.equal(moment([2011, 0, 1]).format('DDDo'), '1d', '1d');
+    assert.equal(moment([2011, 0, 2]).format('DDDo'), '2na', '2na');
+    assert.equal(moment([2011, 0, 3]).format('DDDo'), '3mh', '3mh');
+    assert.equal(moment([2011, 0, 4]).format('DDDo'), '4mh', '4mh');
+    assert.equal(moment([2011, 0, 5]).format('DDDo'), '5mh', '5mh');
+    assert.equal(moment([2011, 0, 6]).format('DDDo'), '6mh', '6mh');
+    assert.equal(moment([2011, 0, 7]).format('DDDo'), '7mh', '7mh');
+    assert.equal(moment([2011, 0, 8]).format('DDDo'), '8mh', '8mh');
+    assert.equal(moment([2011, 0, 9]).format('DDDo'), '9mh', '9mh');
+    assert.equal(moment([2011, 0, 10]).format('DDDo'), '10mh', '10mh');
+    assert.equal(moment([2011, 0, 11]).format('DDDo'), '11mh', '11mh');
+    assert.equal(moment([2011, 0, 12]).format('DDDo'), '12na', '12na');
+    assert.equal(moment([2011, 0, 13]).format('DDDo'), '13mh', '13mh');
+    assert.equal(moment([2011, 0, 14]).format('DDDo'), '14mh', '14mh');
+    assert.equal(moment([2011, 0, 15]).format('DDDo'), '15mh', '15mh');
+    assert.equal(moment([2011, 0, 16]).format('DDDo'), '16mh', '16mh');
+    assert.equal(moment([2011, 0, 17]).format('DDDo'), '17mh', '17mh');
+    assert.equal(moment([2011, 0, 18]).format('DDDo'), '18mh', '18mh');
+    assert.equal(moment([2011, 0, 19]).format('DDDo'), '19mh', '19mh');
+    assert.equal(moment([2011, 0, 20]).format('DDDo'), '20mh', '20mh');
+    assert.equal(moment([2011, 0, 21]).format('DDDo'), '21mh', '21mh');
+    assert.equal(moment([2011, 0, 22]).format('DDDo'), '22na', '22na');
+    assert.equal(moment([2011, 0, 23]).format('DDDo'), '23mh', '23mh');
+    assert.equal(moment([2011, 0, 24]).format('DDDo'), '24mh', '24mh');
+    assert.equal(moment([2011, 0, 25]).format('DDDo'), '25mh', '25mh');
+    assert.equal(moment([2011, 0, 26]).format('DDDo'), '26mh', '26mh');
+    assert.equal(moment([2011, 0, 27]).format('DDDo'), '27mh', '27mh');
+    assert.equal(moment([2011, 0, 28]).format('DDDo'), '28mh', '28mh');
+    assert.equal(moment([2011, 0, 29]).format('DDDo'), '29mh', '29mh');
+    assert.equal(moment([2011, 0, 30]).format('DDDo'), '30mh', '30mh');
+    assert.equal(moment([2011, 0, 31]).format('DDDo'), '31mh', '31mh');
+});
 
-// test('format month', function (assert) {
-//     var expected = months;
-//     for (var i = 0; i < expected.length; i++) {
-//         assert.equal(moment([2011, i, 1]).format('MMMM,MMM'), expected[i], expected[i]);
-//     }
-// });
+test('format month', function (assert) {
+    var expected = months;
+    for (var i = 0; i < expected.length; i++) {
+        assert.equal(moment([2011, i, 1]).format('MMMM,MMM'), expected[i], expected[i]);
+    }
+});
 
 // test('format week', function (assert) {
 //     var expected = ['Didòmhnaich Did Dò', 'Diluain Dil Lu', 'Dimàirt Dim Mà', 'Diciadain Dic Ci', 'Diardaoin Dia Ar', 'Dihaoine Dih Ha', 'Disathairne Dis Sa'];
