902ba481ddc633a3dac271764e3ce99020ba027f
Martin McWhorter <martin@mcwhorter.org>
2019-01-18 03:11:56 +0000

    [bugfix] locale/en-ie.js date format (#4849)
    * Update locale/en-ie.js
    
    Correct to en-IE date locale to match the ICU definition: http://www.localeplanet.com/icu/en-IE/index.html
    
    * Revert to orgininal
    
    * Update unit test

--- src/locale/en-ie.js
+++ src/locale/en-ie.js
@@ -13,7 +13,7 @@ export default moment.defineLocale('en-ie', {
     longDateFormat : {
         LT : 'HH:mm',
         LTS : 'HH:mm:ss',
-        L : 'DD-MM-YYYY',
+        L : 'DD/MM/YYYY',
         LL : 'D MMMM YYYY',
         LLL : 'D MMMM YYYY HH:mm',
         LLLL : 'dddd D MMMM YYYY HH:mm'
