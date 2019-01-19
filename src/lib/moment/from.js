f4c775a806a454c3f960ae4ef36924a2199f2d73
timothy searcy <timmyzsearcy@gmail.com>
2019-01-19 01:58:27 -0800

    revert from.js

--- src/lib/moment/from.js
+++ src/lib/moment/from.js
@@ -2,16 +2,16 @@ import { createDuration } from '../duration/create';
 import { createLocal } from '../create/local';
 import { isMoment } from '../moment/constructor';
 
-export function from (time, withoutSuffix, includeWeeks) {
+export function from (time, withoutSuffix) {
     if (this.isValid() &&
             ((isMoment(time) && time.isValid()) ||
              createLocal(time).isValid())) {
-        return createDuration({to: this, from: time}).locale(this.locale()).humanize(!withoutSuffix, includeWeeks);
+        return createDuration({to: this, from: time}).locale(this.locale()).humanize(!withoutSuffix);
     } else {
         return this.localeData().invalidDate();
     }
 }
 
-export function fromNow (withoutSuffix, includeWeeks) {
-    return this.from(createLocal(), withoutSuffix, includeWeeks);
+export function fromNow (withoutSuffix) {
+    return this.from(createLocal(), withoutSuffix);
 }
