29f2264a734e96adb87689c3d22daabb1a574bd3
Kunal Marwaha <marwahaha@berkeley.edu>
2019-01-20 13:35:35 -0800

    Fix calendar test

--- src/test/locale/oc-lnc.js
+++ src/test/locale/oc-lnc.js
@@ -181,7 +181,7 @@ test('calendar last week', function (assert) {
         m = moment().subtract({d: i});
         assert.equal(m.calendar(),       m.format('dddd [passat a] LT'),  'Today - ' + i + ' days current time');
         m.hours(0).minutes(0).seconds(0).milliseconds(0);
-        assert.equal(m.calendar(),       m.format('dddd [passat a]'),  'Today - ' + i + ' days beginning of day');
+        assert.equal(m.calendar(),       m.format('dddd [passat a] LT'),  'Today - ' + i + ' days beginning of day');
         m.hours(23).minutes(59).seconds(59).milliseconds(999);
         assert.equal(m.calendar(),       m.format('dddd [passat a] LT'),  'Today - ' + i + ' days end of day');
     }
