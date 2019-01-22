6195bb2e14d6cb6a11a476957bc2018c3c28edde
Kunal Marwaha <marwahaha@berkeley.edu>
2019-01-21 18:33:41 -0800

    Revert non-core changes

--- src/test/moment/format.js
+++ src/test/moment/format.js
@@ -462,7 +462,7 @@ test('quarter ordinal formats', function (assert) {
 
 //     var locales =
 //         'ar-sa ar-tn ar az be bg bn bo br bs ca cs cv cy da de-at de dv el ' +
-//         'en-au en-ca en-gb en-ie en-nz eo es et eu fa fi fil fo fr-ca fr-ch fr fy ' +
+//         'en-au en-ca en-gb en-ie en-nz eo es et eu fa fi fo fr-ca fr-ch fr fy ' +
 //         'gd gl he hi hr hu hy-am id is it ja jv ka kk km ko lb lo lt lv me mk ml ' +
 //         'mr ms-my ms my nb ne nl nn pl pt-br pt ro ru se si sk sl sq sr-cyrl ' +
 //         'sr sv sw ta te th tl-ph tlh tr tzl tzm-latn tzm uk uz vi zh-cn zh-tw';
