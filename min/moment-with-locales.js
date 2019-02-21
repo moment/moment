285390d6c37b006ea397b53d2a618ea352c1c247
Daniel Schuetter <daniel@gigwell.com>
2019-02-20 16:08:44 -0800

    add custom localFormattingToken LLLLL

--- min/moment-with-locales.js
+++ min/moment-with-locales.js
@@ -543,7 +543,7 @@
 
     var formattingTokens = /(\[[^\[]*\])|(\\)?([Hh]mm(ss)?|Mo|MM?M?M?|Do|DDDo|DD?D?D?|ddd?d?|do?|w[o|w]?|W[o|W]?|Qo?|YYYYYY|YYYYY|YYYY|YY|gg(ggg?)?|GG(GGG?)?|e|E|a|A|hh?|HH?|kk?|mm?|ss?|S{1,9}|x|X|zz?|ZZ?|.)/g;
 
-    var localFormattingTokens = /(\[[^\[]*\])|(\\)?(LTS|LT|LL?L?L?|l{1,4})/g;
+    var localFormattingTokens = /(\[[^\[]*\])|(\\)?(LTS|LT|LL?L?L?L?|l{1,5})/g;
 
     var formatFunctions = {};
 
