f71f5f3e363705d7cf6bca97bc784806e3cf804e
Orif N. Jr <orif.zade@gmail.com>
2019-04-03 13:48:08 +0200

    fix long date formats

--- src/locale/tg.js
+++ src/locale/tg.js
@@ -30,7 +30,10 @@ var suffixes = {
 };
 
 export default moment.defineLocale('tg', {
-    months : 'январ_феврал_март_апрел_май_июн_июл_август_сентябр_октябр_ноябр_декабр'.split('_'),
+    months : {
+        format: 'январи_феврали_марти_апрели_майи_июни_июли_августи_сентябри_октябри_ноябри_декабри'.split('_'),
+        standalone: 'январ_феврал_март_апрел_май_июн_июл_август_сентябр_октябр_ноябр_декабр'.split('_')
+    },
     monthsShort : 'янв_фев_мар_апр_май_июн_июл_авг_сен_окт_ноя_дек'.split('_'),
     weekdays : 'якшанбе_душанбе_сешанбе_чоршанбе_панҷшанбе_ҷумъа_шанбе'.split('_'),
     weekdaysShort : 'яшб_дшб_сшб_чшб_пшб_ҷум_шнб'.split('_'),
@@ -38,10 +41,10 @@ export default moment.defineLocale('tg', {
     longDateFormat : {
         LT : 'HH:mm',
         LTS : 'HH:mm:ss',
-        L : 'DD/MM/YYYY',
-        LL : 'D MMMM YYYY',
-        LLL : 'D MMMM YYYY HH:mm',
-        LLLL : 'dddd, D MMMM YYYY HH:mm'
+        L : 'DD.MM.YYYY',
+        LL : 'D MMMM YYYY c.',
+        LLL : 'D MMMM YYYY c. HH:mm',
+        LLLL : 'dddd, D MMMM YYYY с. HH:mm'
     },
     calendar : {
         sameDay : '[Имрӯз соати] LT',
