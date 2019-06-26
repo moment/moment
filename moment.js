d6c745e7075b67ec2fab4a13bc994429c315ede5
Caio Shimada Rabello <xcaiosr@gmail.com>
2019-06-26 17:01:26 -0300

    Add Header comment with Current Version
    Closes #5057

--- moment.js
+++ moment.js
@@ -1,4 +1,8 @@
 //! moment.js
+//! version : 2.24.0
+//! authors : Tim Wood, Iskren Chernev, Moment.js contributors
+//! license : MIT
+//! momentjs.com
 
 ;(function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
