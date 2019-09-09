e9477741966ef8da8bc8cfc5ad6e8bd3d1727b27
ossdev <ossdev@puresoftware.com>
2019-09-09 06:09:20 +0000

    moment: removed spacejam Removed spacejam
    Signed-off-by: ossdev <ossdev@puresoftware.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -165,10 +165,6 @@ module.exports = function (grunt) {
                 command:
                     'type meteor >/dev/null 2>&1 || { curl https://install.meteor.com/ | sh; }',
             },
-            'meteor-test': {
-                command:
-                    'spacejam --mongo-url mongodb:// test-packages ./meteor',
-            },
             'meteor-publish': {
                 command: 'cd meteor && meteor publish',
             },
@@ -252,7 +248,6 @@ module.exports = function (grunt) {
     ]);
     grunt.registerTask('test:meteor', [
         'exec:meteor-init',
-        'exec:meteor-test',
         'exec:meteor-cleanup',
     ]);
 
