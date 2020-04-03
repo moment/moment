12bf4ececfab744fc463ae4f6f11d4e555869450
Chamie <mail@chamie.fr>
2020-04-03 23:57:45 +0200

    typo apostrophe instead of simple quote
    Should be  (ʼ, U+02BC) 
    
    https://www.unicode.org/cldr/charts/latest/summary/br.html
    https://fr.wikipedia.org/wiki/Apostrophe_(typographie)#En_breton

--- src/locale/br.js
+++ src/locale/br.js
@@ -49,9 +49,9 @@ function softMutation(text) {
 }
 
 export default moment.defineLocale('br', {
-    months : 'Genver_C\'hwevrer_Meurzh_Ebrel_Mae_Mezheven_Gouere_Eost_Gwengolo_Here_Du_Kerzu'.split('_'),
-    monthsShort : 'Gen_C\'hwe_Meu_Ebr_Mae_Eve_Gou_Eos_Gwe_Her_Du_Ker'.split('_'),
-    weekdays : 'Sul_Lun_Meurzh_Merc\'her_Yaou_Gwener_Sadorn'.split('_'),
+    months : 'Genver_Cʼhwevrer_Meurzh_Ebrel_Mae_Mezheven_Gouere_Eost_Gwengolo_Here_Du_Kerzu'.split('_'),
+    monthsShort : 'Gen_Cʼhwe_Meu_Ebr_Mae_Eve_Gou_Eos_Gwe_Her_Du_Ker'.split('_'),
+    weekdays : 'Sul_Lun_Meurzh_Mercʼher_Yaou_Gwener_Sadorn'.split('_'),
     weekdaysShort : 'Sul_Lun_Meu_Mer_Yao_Gwe_Sad'.split('_'),
     weekdaysMin : 'Su_Lu_Me_Mer_Ya_Gw_Sa'.split('_'),
     weekdaysParseExact : true,
@@ -65,9 +65,9 @@ export default moment.defineLocale('br', {
     },
     calendar : {
         sameDay : '[Hiziv da] LT',
-        nextDay : '[Warc\'hoazh da] LT',
+        nextDay : '[Warcʼhoazh da] LT',
         nextWeek : 'dddd [da] LT',
-        lastDay : '[Dec\'h da] LT',
+        lastDay : '[Decʼh da] LT',
         lastWeek : 'dddd [paset da] LT',
         sameElse : 'L'
     },
