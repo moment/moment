56c5048018635032266aea099e2dd55723bcb20e
Petter-Byggmann <57890308+Petter-Byggmann@users.noreply.github.com>
2020-04-23 10:26:16 +0200

    Update nb.js
    Should have the same short month format as nn.js

--- locale/nb.js
+++ locale/nb.js
@@ -10,7 +10,7 @@
 
     var nb = moment.defineLocale('nb', {
         months : 'januar_februar_mars_april_mai_juni_juli_august_september_oktober_november_desember'.split('_'),
-        monthsShort : 'jan._feb._mars_april_mai_juni_juli_aug._sep._okt._nov._des.'.split('_'),
+        monthsShort : 'jan_feb_mar_apr_mai_jun_jul_aug_sep_okt_nov_des'.split('_'),
         monthsParseExact : true,
         weekdays : 'søndag_mandag_tirsdag_onsdag_torsdag_fredag_lørdag'.split('_'),
         weekdaysShort : 'sø._ma._ti._on._to._fr._lø.'.split('_'),
