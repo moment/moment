ebfd8dae146cd8ec1cb4943cd663eda75a720b82
Alex Mitchell <alex+github@alexmitchelltech.com>
2020-04-25 17:30:25 -0500

    [bugfix] Use ISO Year for weeksInISOYear instead of calendar year. (#4410)
    * Use calendar year of day at end of week for ISO year week count
    
    * Fix ambiguously constructed test moments
    
    * Add some spacing

--- src/lib/units/week-year.js
+++ src/lib/units/week-year.js
@@ -8,6 +8,10 @@ import toInt from '../utils/to-int';
 import { hooks } from '../utils/hooks';
 import { createUTCDate } from '../create/date-from-array';
 
+// CONSTANTS
+
+const THURSDAY = 4;
+
 // FORMATTING
 
 addFormatToken(0, ['gg', 2], 0, function () {
@@ -74,7 +78,7 @@ export function getSetISOWeekYear (input) {
 }
 
 export function getISOWeeksInYear () {
-    return weeksInYear(this.year(), 1, 4);
+    return weeksInYear(this.isoWeekday(THURSDAY).year(), 1, 4);
 }
 
 export function getWeeksInYear () {
