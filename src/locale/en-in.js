04b275c44e5ffd6b6936b82b59601f515b21b952
Kumar Harsh <kumar.harsh@live.com>
2020-05-19 17:13:54 +0530

    [locale] Fix Indian week start date and week number (#5502)

--- src/locale/en-in.js
+++ src/locale/en-in.js
@@ -62,7 +62,7 @@ export default moment.defineLocale('en-in', {
         return number + output;
     },
     week: {
-        dow: 1, // Monday is the first day of the week.
-        doy: 4, // The week that contains Jan 4th is the first week of the year.
+        dow: 0, // Sunday is the first day of the week.
+        doy: 6, // The week that contains Jan 1st is the first week of the year.
     },
 });
