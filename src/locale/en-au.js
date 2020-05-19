94af7ff232aa3b4e985e122e26851c704dbd5d73
Kunal Marwaha <marwahaha@berkeley.edu>
2020-05-19 05:09:39 -0700

    Update en-au.js

--- src/locale/en-au.js
+++ src/locale/en-au.js
@@ -63,6 +63,6 @@ export default moment.defineLocale('en-au', {
     },
     week: {
         dow: 0, // Sunday is the first day of the week.
-        doy: 4,  // The week that contains Jan 4th is the first week of the year.
+        doy: 4, // The week that contains Jan 4th is the first week of the year.
     },
 });
