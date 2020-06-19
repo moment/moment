313b798f187bc5910de9329eff2a49ff7c294967
Kafil <kafil.nasdami@gmail.com>
2020-06-19 20:11:04 +0100

    The week that contains Jan 4th is the first week of the year

--- locale/ar-ma.js
+++ locale/ar-ma.js
@@ -57,7 +57,7 @@
         },
         week: {
             dow: 6, // Saturday is the first day of the week.
-            doy: 12, // The week that contains Jan 12th is the first week of the year.
+            doy: 4, // The week that contains Jan 4th is the first week of the year.
         },
     });
 
