8980b5bde5eabfbad51bd2a072f1e38ae9616261
Adam Beaton <ambeaton3@gmail.com>
2020-06-21 13:27:52 -0400

    Add tests for isLeapYear, including edge cases

--- src/lib/utils/is-leap-year.js
+++ src/lib/utils/is-leap-year.js
@@ -1,3 +1,14 @@
+/**
+ * Returns true if the year corresponds to a leap year in the
+ * past, present, or future.
+ * @param {*} year a year utilized as if it were a number
+ * @returns whether or not the provided year is a leap year in the
+ * Gregorian calendar
+ */
 export function isLeapYear(year) {
+    if (isNaN(year) || !isFinite(year)) {
+        return false;
+    }
+
     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
