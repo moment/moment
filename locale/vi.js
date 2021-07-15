c1b2646de4632b94926e4e71098f14700444ca13
Nobi <nobi@nobidev.com>
2021-07-16 05:16:08 +0700

    Fix case style for the Vietnamese language

--- locale/vi.js
+++ locale/vi.js
@@ -13,14 +13,14 @@
     //! moment.js locale configuration
 
     var vi = moment.defineLocale('vi', {
-        months: 'tháng 1_tháng 2_tháng 3_tháng 4_tháng 5_tháng 6_tháng 7_tháng 8_tháng 9_tháng 10_tháng 11_tháng 12'.split(
+        months: 'Tháng 1_Tháng 2_Tháng 3_Tháng 4_Tháng 5_Tháng 6_Tháng 7_Tháng 8_Tháng 9_Tháng 10_Tháng 11_Tháng 12'.split(
             '_'
         ),
         monthsShort: 'Thg 01_Thg 02_Thg 03_Thg 04_Thg 05_Thg 06_Thg 07_Thg 08_Thg 09_Thg 10_Thg 11_Thg 12'.split(
             '_'
         ),
         monthsParseExact: true,
-        weekdays: 'chủ nhật_thứ hai_thứ ba_thứ tư_thứ năm_thứ sáu_thứ bảy'.split(
+        weekdays: 'Chủ nhật_Thứ hai_Thứ ba_Thứ tư_Thứ năm_Thứ sáu_Thứ bảy'.split(
             '_'
         ),
         weekdaysShort: 'CN_T2_T3_T4_T5_T6_T7'.split('_'),
@@ -52,9 +52,9 @@
         calendar: {
             sameDay: '[Hôm nay lúc] LT',
             nextDay: '[Ngày mai lúc] LT',
-            nextWeek: 'dddd [tuần tới lúc] LT',
+            nextWeek: 'dddd [Tuần tới lúc] LT',
             lastDay: '[Hôm qua lúc] LT',
-            lastWeek: 'dddd [tuần trước lúc] LT',
+            lastWeek: 'dddd [Tuần trước lúc] LT',
             sameElse: 'L',
         },
         relativeTime: {
