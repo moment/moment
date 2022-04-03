5a2987758edc7d413d1248737d9d0d1b65a70450
Iskren Chernev <me@iskren.info>
2022-04-03 16:10:39 +0300

    Bump version to 2.29.2

--- meteor/package.js
+++ meteor/package.js
@@ -6,7 +6,7 @@ var packageName = 'momentjs:moment';  // https://atmospherejs.com/momentjs/momen
 Package.describe({
   name: packageName,
   summary: 'Moment.js (official): parse, validate, manipulate, and display dates - official Meteor packaging',
-  version: '2.29.1',
+  version: '2.29.2',
   git: 'https://github.com/moment/moment.git'
 });
 
