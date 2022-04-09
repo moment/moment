36bc3271ce8e8651ccbba0b101de425fbffa084b
Jonas Rodrigues <jonas.herique90@gmail.com>
2022-04-08 23:50:59 -0300

    fixes weekdaysMin on locale

--- locale/pt-br.js
+++ locale/pt-br.js
@@ -21,7 +21,7 @@
                 '_'
             ),
         weekdaysShort: 'dom_seg_ter_qua_qui_sex_sáb'.split('_'),
-        weekdaysMin: 'd_s_t_q_q_s_s'.split('_'),
+        weekdaysMin: 'do_2ª_3ª_4ª_5ª_6ª_sá'.split('_'),
         weekdaysParseExact: true,
         longDateFormat: {
             LT: 'HH:mm',
