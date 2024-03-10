f263776bd4bc7a5acd2cac8de4014015bd532848
Adi Levy <adidas.levy@gmail.com>
2024-03-10 17:32:25 +0200

    [locale] Fixed ('L') format for Georgian and Flemish (Belgium Dutch). Done according to: Georgian: https://www.localeplanet.com/icu/ka/index.html Dutch(Belgium): https://www.localeplanet.com/icu/nl-BE/index.html

--- src/locale/nl-be.js
+++ src/locale/nl-be.js
@@ -59,7 +59,7 @@ export default moment.defineLocale('nl-be', {
     longDateFormat: {
         LT: 'HH:mm',
         LTS: 'HH:mm:ss',
-        L: 'DD/MM/YYYY',
+        L: 'D/MM/YYYY',
         LL: 'D MMMM YYYY',
         LLL: 'D MMMM YYYY HH:mm',
         LLLL: 'dddd D MMMM YYYY HH:mm',
