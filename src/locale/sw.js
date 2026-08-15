d852c9f0eade93f3364f387c93a275df6dd71ef7
Matt Johnson-Pint <mattjohnsonpint@gmail.com>
2026-08-14 17:11:52 -0700

    [locale] Fix Swahili relative weekday word order

--- src/locale/sw.js
+++ src/locale/sw.js
@@ -27,9 +27,9 @@ export default moment.defineLocale('sw', {
     calendar: {
         sameDay: '[leo saa] LT',
         nextDay: '[kesho saa] LT',
-        nextWeek: '[wiki ijayo] dddd [saa] LT',
+        nextWeek: 'dddd [ijayo saa] LT',
         lastDay: '[jana] LT',
-        lastWeek: '[wiki iliyopita] dddd [saa] LT',
+        lastWeek: 'dddd [iliyopita saa] LT',
         sameElse: 'L',
     },
     relativeTime: {
