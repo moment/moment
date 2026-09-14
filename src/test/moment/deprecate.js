ff0f5266d2bbd8f3cae7ffeadca607c1a8dd005f
Matt Johnson-Pint <mattjohnsonpint@gmail.com>
2026-09-14 09:24:52 -0600

    [bugfix] Add stack traces to conditional deprecations

--- src/test/moment/deprecate.js
+++ src/test/moment/deprecate.js
@@ -1,5 +1,5 @@
 import { module, test } from '../qunit';
-import { deprecate } from '../../lib/utils/deprecate';
+import { deprecate, deprecateSimple } from '../../lib/utils/deprecate';
 import { hooks } from '../../lib/utils/hooks';
 
 module('deprecate');
@@ -15,4 +15,35 @@ test('deprecate', function (assert) {
     hooks.suppressDeprecationWarnings = oldSuppressDeprecationWarnings;
 
     assert.expect(0);
+});
+
+test('deprecateSimple includes a stack trace and warns once', function (assert) {
+    var oldSuppressDeprecationWarnings = hooks.suppressDeprecationWarnings,
+        oldWarn = console.warn,
+        warnings = [];
+
+    hooks.suppressDeprecationWarnings = false;
+    console.warn = function (msg) {
+        warnings.push(msg);
+    };
+
+    try {
+        deprecateSimple(
+            'testing deprecateSimple',
+            'testing simple deprecation'
+        );
+        deprecateSimple(
+            'testing deprecateSimple',
+            'testing simple deprecation'
+        );
+    } finally {
+        console.warn = oldWarn;
+        hooks.suppressDeprecationWarnings = oldSuppressDeprecationWarnings;
+    }
+
+    assert.equal(warnings.length, 1, 'warns once for each named deprecation');
+    assert.ok(
+        warnings[0].indexOf('testing simple deprecation\n') !== -1,
+        'includes a stack trace after the message'
+    );
 });
